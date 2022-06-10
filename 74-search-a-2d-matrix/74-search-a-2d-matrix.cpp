323770def5d18e6bd796fc8643458bbe7d52bb15
Ankit D <a.singh725098@gmail.com>
2022-06-10 12:03:51 +0530

    Time: 8 ms (36.58%), Space: 9.6 MB (52.58%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -11,17 +11,9 @@ public:
         while(left<=right)
         {
             int mid=left+(right-left)/2;
-            if(matrix[mid/m][mid%m]==target)
-                return(true);
-            else if(matrix[mid/m][mid%m]>target)
-            {
-                if((mid/m==0) && (mid%m==0))return(false);
-                else right=mid-1;
-            }
-            else {
-                if((mid/m==n-1) && (mid%m==m-1))return(false);
-                else left=mid+1;
-            }
+            if(matrix[mid/m][mid%m]==target) return(true);
+            else if(matrix[mid/m][mid%m]>target) right=mid-1;
+            else left=mid+1;   
         }
         
         return(false);
