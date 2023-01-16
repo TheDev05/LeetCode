04e9c186b458c56e9f405c4501b4377f66e6731e
Ankit D <a.singh725098@gmail.com>
2023-01-16 23:25:44 +0530

    Time: 3 ms (98.16%), Space: 8.9 MB (53.66%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    int getCount(std::vector<std::vector<int>> &num, std::vector<std::vector<int>> &storage, int i, int j)
-{
-    if (i == num.size() - 1)
-        return num[i][j];
-    if (j > i)
-        return INT_MAX;
-
-    if (storage[i][j] == -1)
-    {
-        int down = getCount(num, storage, i + 1, j);
-        int diagonal = getCount(num, storage, i + 1, j + 1);
-
-        storage[i][j] = std::min(down, diagonal) + num[i][j];
-    }
-
-    return storage[i][j];
-}
-
     int minimumTotal(vector<vector<int>>& num) {
         
-    std::vector<std::vector<int>> storage;
-    for (int i = 0; i < num.size(); i++)
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(num.size(), -1));
+    for (int i = 0; i < storage[storage.size() - 1].size(); i++)
     {
-        std::vector<int> temp(i + 1);
-        for (int j = 0; j <= i; j++)
-        {
-            temp[j] = -1;
-        }
-
-        storage.push_back(temp);
+        storage[storage.size() - 1][i] = num[num.size() - 1][i];
     }
 
-    return getCount(num, storage, 0, 0);
+    for (int i = num.size() - 2; i >= 0; i--)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            int down = storage[i + 1][j];
+            int diagonal = storage[i + 1][j + 1];
+
+            storage[i][j] = std::min(down, diagonal) + num[i][j];
+        }
+    }
+
+    return storage[0][0];
         
     }
 };
