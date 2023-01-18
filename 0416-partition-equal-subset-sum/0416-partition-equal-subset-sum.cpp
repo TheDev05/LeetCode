4cade93a477b7c8b1ee86224b83ec19a3945d60f
Ankit D <a.singh725098@gmail.com>
2023-01-18 21:32:12 +0530

    Time: 170 ms (82.72%), Space: 10.4 MB (84.20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int sum, int k, int index)
-{
-    if (index >= num.size())
-    {
-        if (k == sum)
-            return true;
-        return false;
-    }
-
-    if (storage[index][sum] == -1)
-    {
-        bool val1 = false;
-        if(sum + num[index] <= k)
-            val1 = getCount(num, storage, sum + num[index], k, index + 1);
-        bool val2 = getCount(num, storage, sum, k, index + 1);
-        
-        storage[index][sum] = val1 | val2;
-    }
-
-    return storage[index][sum];
-}
-
     bool canPartition(vector<int>& num) {
         
+    int n=num.size();
     int k = accumulate(num.begin(), num.end(), 0);
-    if(k & 1) return false;
-    else k = k / 2;
         
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(k+1, -1));
+    if (k & 1)
+        return false;
 
-    return getCount(num, storage, 0, k, 0);
+    k = k / 2;
+
+       std::vector<int> storage(k + 1, false);
+    storage[storage.size() - 1] = true;
+
+    bool result = false;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < storage.size(); j++)
+        {
+            int index = j + num[i];
+            if (index >= storage.size())
+                continue;
+
+            if (storage[index])
+            {
+                storage[j] = true;
+            }
+        }
+    }
+
+    return storage[0];
         
     }
 };
