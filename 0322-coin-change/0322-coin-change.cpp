1ccad202f86042b6aa2b11e34806f6eaa9256cb1
Ankit D <a.singh725098@gmail.com>
2023-01-20 19:23:10 +0530

    Time: 165 ms (34.51%), Space: 49.2 MB (16.40%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int k, int index)
-{
-    if (index >= num.size())
-    {
-        if (k == 0)
-            return 0;
-
-        return INT_MAX - 1;
-    }
-
-    if (storage[index][k] == -1)
-    {
-        int val1 = INT_MAX, val2 = INT_MAX;
-        if (k - num[index] >= 0)
-        {
-            val1 = 1 + getCount(num, storage, k - num[index], index);
-        }
-
-        val2 = getCount(num, storage, k, index + 1);
-        storage[index][k] = std::min(val1, val2);
-    }
-
-    return storage[index][k];
-}
-
     int coinChange(vector<int>& num, int k) {
         
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(k + 1, -1));
-    int res = getCount(num, storage, k, 0);
+    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(k + 1, 1e8));
+    storage[storage.size() - 1][0] = 0;
+    
+    for (int i = storage.size() - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            int val1 = 1e8, val2 = 1e8;
+            if (j - num[i] >= 0)
+                val1 = 1 + storage[i][j - num[i]];
+            val2 = storage[i + 1][j];
 
-    if (res == INT_MAX-1)
-        return -1;
+            storage[i][j] = std::min(val1, val2);
+        }
+    }
+
+    int res = storage[0][k];
+    if(res==1e8)res=-1;
+        
     return res;
         
     }
