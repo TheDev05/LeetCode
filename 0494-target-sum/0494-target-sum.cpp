b3a042a28b605b4308e4b1d4a2d1999dfa6f4c94
Ankit D <a.singh725098@gmail.com>
2023-01-21 11:09:09 +0530

    Time: 11 ms (89.51%), Space: 12.2 MB (43.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int getCount(std::vector<int> &num, int sum, int index)
+    
+int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int sum, int index)
 {
     if (index >= num.size())
         return (sum == 0) ? 1 : 0;
 
-    int val1 = getCount(num, sum - num[index], index + 1);
-    int val2 = getCount(num, sum, index + 1);
+    if (storage[index][sum] == -1)
+    {
+        int val1 = 0, val2 = 0;
+        if (sum - num[index] >= 0)
+            val1 = getCount(num, storage, sum - num[index], index + 1);
+        val2 = getCount(num, storage, sum, index + 1);
 
-    return val1 + val2;
+        storage[index][sum] = val1 + val2;
+    }
+
+    return storage[index][sum];
 }
 
+    
     int findTargetSumWays(vector<int>& num, int k) {
-            int total = 0, sum = 0;
+    int total = 0, sum = 0;
     for (auto i : num)
         total += i;
+        
+    if (k < -total || k > total)
+        return 0;
 
     if ((total + k) % 2 == 0)
     {
@@ -25,6 +37,8 @@ public:
         sum = (total - k) / 2;
     }else return 0;
 
-    return getCount(num, sum, 0);
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(sum + 1, -1));
+    return getCount(num, storage, sum, 0); 
+        
     }
 };
