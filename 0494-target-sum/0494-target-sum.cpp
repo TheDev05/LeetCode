37b96b7ca4902029894e8432b441ae38fd93fcad
Ankit D <a.singh725098@gmail.com>
2023-01-21 11:15:06 +0530

    Time: 16 ms (76.68%), Space: 12.5 MB (36.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    
-int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int sum, int index)
-{
-    if (index >= num.size())
-        return (sum == 0) ? 1 : 0;
-
-    if (storage[index][sum] == -1)
-    {
-        int val1 = 0, val2 = 0;
-        if (sum - num[index] >= 0)
-            val1 = getCount(num, storage, sum - num[index], index + 1);
-        val2 = getCount(num, storage, sum, index + 1);
-
-        storage[index][sum] = val1 + val2;
-    }
-
-    return storage[index][sum];
-}
-
-    
+       
     int findTargetSumWays(vector<int>& num, int k) {
     int total = 0, sum = 0;
     for (auto i : num)
@@ -37,8 +18,23 @@ int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int 
         sum = (total - k) / 2;
     }else return 0;
 
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(sum + 1, -1));
-    return getCount(num, storage, sum, 0); 
+    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(sum + 1, 0));
+    storage[storage.size() - 1][0] = 1;
+    
+    for (int i = storage.size() - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            int val1 = 0, val2 = 0;
+            if (j - num[i] >= 0)
+                val1 = storage[i + 1][j - num[i]];
+            val2 = storage[i + 1][j];
+
+            storage[i][j] = val1 + val2;
+        }
+    }
+
+    return storage[0][sum]; 
         
     }
 };
