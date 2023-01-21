88f1e4e5fa326c6fdac1582c3d1bbafa095ff0dd
Ankit D <a.singh725098@gmail.com>
2023-01-21 11:18:51 +0530

    Time: 10 ms (91.12%), Space: 9.4 MB (57.90%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -18,23 +18,25 @@ public:
         sum = (total - k) / 2;
     }else return 0;
 
-    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(sum + 1, 0));
-    storage[storage.size() - 1][0] = 1;
-    
-    for (int i = storage.size() - 2; i >= 0; i--)
+    std::vector<int> prev(sum + 1, 0), curr(sum + 1, 0);
+    prev[0] = 1;
+
+    for (int i = num.size() - 1; i >= 0; i--)
     {
-        for (int j = 0; j < storage[0].size(); j++)
+        for (int j = 0; j < prev.size(); j++)
         {
             int val1 = 0, val2 = 0;
             if (j - num[i] >= 0)
-                val1 = storage[i + 1][j - num[i]];
-            val2 = storage[i + 1][j];
+                val1 = prev[j - num[i]];
+            val2 = prev[j];
 
-            storage[i][j] = val1 + val2;
+            curr[j] = val1 + val2;
         }
+
+        prev = curr;
     }
 
-    return storage[0][sum]; 
+    return curr[sum];
         
     }
 };
