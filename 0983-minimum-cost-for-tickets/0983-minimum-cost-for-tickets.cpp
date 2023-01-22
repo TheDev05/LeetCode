4537c5a4f5ad5211fa2f6678611ab94fd869334d
Ankit D <a.singh725098@gmail.com>
2023-01-22 22:56:03 +0530

    Time: 64 ms (5.11%), Space: 19.4 MB (6.52%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-int getCount(std::vector<int> &days, std::vector<int> &costs, std::vector<std::vector<int>> &storage, int hold, int index)
-{
-    if (index >= days.size())
-        return 0;
-
-    if (storage[index][hold] == -1)
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    std::vector<std::vector<int>> storage(days.size() + 1, std::vector<int>(366, 0));
+    for (int i = storage.size() - 2; i >= 0; i--)
     {
-        int val1 = costs[0], val2 = costs[1], val3 = costs[2];
-        if (days[index] < hold)
-            return (getCount(days, costs, storage, hold, index + 1));
-        else
+        for (int j = 0; j < storage[0].size(); j++)
         {
-            if (days[index] + 1 < 366)
-                val1 += getCount(days, costs, storage, days[index] + 1, index + 1);
-            if (days[index] + 7 < 366)
-                val2 += getCount(days, costs, storage, days[index] + 7, index + 1);
-            if (days[index] + 30 < 366)
-                val3 += getCount(days, costs, storage, days[index] + 30, index + 1);
-        }
+            int val1 = costs[0], val2 = costs[1], val3 = costs[2];
+            if (days[i] < j)
+                storage[i][j] = storage[i + 1][j];
+            else
+            {
+                if (days[i] + 1 < 366)
+                    val1 += storage[i + 1][days[i] + 1];
+                if (days[i] + 7 < 366)
+                    val2 += storage[i + 1][days[i] + 7];
+                if (days[i] + 30 < 366)
+                    val3 += storage[i + 1][days[i] + 30];
 
-        storage[index][hold] = std::min({val1, val2, val3});
+                storage[i][j] = std::min({val1, val2, val3});
+            }
+        }
     }
 
-    return storage[index][hold];
-}
-   
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-    std::vector<std::vector<int>> storage(days.size(), std::vector<int>(366, -1));
-    return getCount(days, costs, storage, 0, 0);
+    return storage[0][0];
     }
 };
