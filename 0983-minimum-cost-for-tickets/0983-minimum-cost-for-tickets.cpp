fe0490a1bd2830bb85a2f6d12a2278ce9b6779e7
Ankit D <a.singh725098@gmail.com>
2023-01-22 23:00:19 +0530

    Time: 112 ms (5.11%), Space: 9.5 MB (77.91%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-    std::vector<std::vector<int>> storage(days.size() + 1, std::vector<int>(366, 0));
-    for (int i = storage.size() - 2; i >= 0; i--)
+        
+    std::vector<int> prev(366, 0);
+    for (int i = days.size() - 1; i >= 0; i--)
     {
-        for (int j = 0; j < storage[0].size(); j++)
+        for (int j = 365; j >= 0; j--)
         {
             int val1 = costs[0], val2 = costs[1], val3 = costs[2];
             if (days[i] < j)
-                storage[i][j] = storage[i + 1][j];
+                prev[j] = prev[j];
             else
             {
                 if (days[i] + 1 < 366)
-                    val1 += storage[i + 1][days[i] + 1];
+                    val1 += prev[days[i] + 1];
                 if (days[i] + 7 < 366)
-                    val2 += storage[i + 1][days[i] + 7];
+                    val2 += prev[days[i] + 7];
                 if (days[i] + 30 < 366)
-                    val3 += storage[i + 1][days[i] + 30];
+                    val3 += prev[days[i] + 30];
 
-                storage[i][j] = std::min({val1, val2, val3});
+                prev[j] = std::min({val1, val2, val3});
             }
         }
     }
 
-    return storage[0][0];
+    return prev[0];
     }
 };
