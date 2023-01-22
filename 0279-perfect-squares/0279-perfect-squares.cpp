4891454511d669847df0cb4d37beee323415c0fc
Ankit D <a.singh725098@gmail.com>
2023-01-22 20:16:54 +0530

    Time: 817 ms (8.97%), Space: 154 MB (5.02%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int k, int index)
-{
-    if (index >= num.size())
-        return (k == 0) ? 0 : 1e7;
-
-    if (storage[index][k] == -1)
-    {
-        int val1 = 1e7, val2 = 1e7;
-        if (k - num[index] >= 0)
-            val1 = 1 + getCount(num, storage, k - num[index], index);
-        val2 = getCount(num, storage, k, index + 1);
-
-        storage[index][k] = std::min(val1, val2);
-    }
-
-    return storage[index][k];
-}
-
-    
-    
     int numSquares(int n) {
         
     std::vector<int> num;
@@ -30,8 +10,23 @@ int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int 
             num.push_back(i);
     }
 
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(n + 1, -1));
-    return getCount(num, storage, n, 0);
+    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(n + 1, 1e7));
+    storage[storage.size() - 1][0] = 0;
+
+    for (int i = storage.size() - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            int val1 = 1e7, val2 = 1e7;
+            if (j - num[i] >= 0)
+                val1 = 1 + storage[i][j - num[i]];
+            val2 = storage[i + 1][j];
+
+            storage[i][j] = std::min(val1, val2);
+        }
+    }
+
+    return storage[0][n];
         
     }
 };
