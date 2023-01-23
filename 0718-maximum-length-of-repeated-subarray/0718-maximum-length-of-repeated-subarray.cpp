eeb28b2d4b3dade05c704a2bbdee76f4a36cdbb9
Ankit D <a.singh725098@gmail.com>
2023-01-24 01:04:34 +0530

    Time: 220 ms (72.77%), Space: 11.1 MB (75.05%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -3,18 +3,18 @@ public:
     int findLength(vector<int>& num1, vector<int>& num2) {
         
     int max = INT_MIN, n=num1.size(), m=num2.size();
-    std::vector<std::vector<int>> storage(n + 1, std::vector<int>(m + 1, 0));
-        
-    for (int i = storage.size() - 2; i >= 0; i--)
+    std::vector<int> prev(m + 1, 0);
+
+    for (int i = n - 1; i >= 0; i--)
     {
-        for (int j = storage[0].size() - 2; j >= 0; j--)
+        for (int j = 0; j < prev.size() - 1; j++)
         {
             if (num1[i] == num2[j])
-                storage[i][j] = 1 + storage[i + 1][j + 1];
+                prev[j] = 1 + prev[j + 1];
             else
-                storage[i][j] = 0;
+                prev[j] = 0;
 
-            max = std::max(max, storage[i][j]);
+            max = std::max(max, prev[j]);
         }
     }
 
