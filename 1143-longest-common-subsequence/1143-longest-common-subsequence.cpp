248237b7ee99a89cecc91aa981d4b32ddea1e0fb
Ankit D <a.singh725098@gmail.com>
2023-01-23 18:19:10 +0530

    Time: 109 ms (5.05%), Space: 18.7 MB (10.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-int getCount(std::string &text1, std::string &text2, std::vector<std::vector<int>> &storage, int index1, int index2)
-{
-    if (index1 >= text1.size() || index2 >= text2.size())
-        return 0;
-
-    if (storage[index1][index2] == -1)
+    int longestCommonSubsequence(string text1, string text2) {
+            std::vector<std::vector<int>> storage(text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+     for (int i = 0; i < storage[0].size(); i++)
     {
-        int val1 = 0, val2 = 0, val3 = 0;
-        if (text1[index1] == text2[index2])
-            val1 = 1 + getCount(text1, text2, storage, index1 + 1, index2 + 1);
-        val2 = getCount(text1, text2, storage, index1 + 1, index2);
-        val3 = getCount(text1, text2, storage, index1, index2 + 1);
-
-        storage[index1][index2] = (std::max({val1, val2, val3}));
+        storage[storage.size() - 1][i] = 0;
     }
 
-    return storage[index1][index2];
-}
-    
-    int longestCommonSubsequence(string text1, string text2) {
-        
-    std::vector<std::vector<int>> storage(text1.size(), std::vector<int>(text2.size(), -1));
-   return getCount(text1, text2, storage, 0, 0);
+    for (int i = 0; i < storage.size(); i++)
+    {
+        storage[i][storage[0].size() - 1] = 0;
+    }
 
+    for (int i = storage.size() - 2; i >= 0; i--)
+    {
+        for (int j = storage[0].size() - 2; j >= 0; j--)
+        {
+            int val1 = 0, val2 = 0, val3 = 0;
+            if (text1[i] == text2[j])
+                val1 = 1 + storage[i + 1][j + 1];
+            val2 = storage[i + 1][j];
+            val3 = storage[i][j + 1];
+
+            storage[i][j] = (std::max({val1, val2, val3}));
+        }
+    }
+
+    return storage[0][0];
     }
 };
