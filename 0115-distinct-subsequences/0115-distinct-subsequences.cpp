3e0ef2257c6cd625c189ad2adce8cc84b4fd4222
Ankit D <a.singh725098@gmail.com>
2023-01-27 19:31:19 +0530

    Time: 30 ms (88.74%), Space: 6.4 MB (96.49%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-int getCount(std::string &text1, std::string &text2, std::vector<std::vector<int>> &storage, int index1, int index2)
-{
-    if (index2 >= text2.size())
-        return 1;
-    else if (index1 >= text1.size())
-        return 0;
+    int numDistinct(string text1, string text2) {
+        
+    std::vector<int> prev(text2.size() + 1, 0);
+    prev[prev.size() - 1] = 1;
 
-    if (storage[index1][index2] == -1)
+    for (int i = text1.size() - 1; i >= 0; i--)
     {
-        int val1 = 0, val2 = 0;
-        if (text1[index1] == text2[index2])
+        for (int j = 0; j < prev.size() - 1; j++)
         {
-            val1 = getCount(text1, text2, storage, index1 + 1, index2 + 1);
-            val2 = getCount(text1, text2, storage, index1 + 1, index2);
-        }
-        else
-            val2 = getCount(text1, text2, storage, index1 + 1, index2);
+            long long val1 = 0, val2 = 0;
+            if (text1[i] == text2[j])
+            {
+                val1 = prev[j + 1];
+                val2 = prev[j];
+            }
+            else
+                val2 = prev[j];
 
-        storage[index1][index2] = (val1 + val2);
+            prev[j] = val1 + val2;
+        }
     }
 
-    return storage[index1][index2];
-}
-    
-    int numDistinct(string text1, string text2) {
-    std::vector<std::vector<int>> storage(text1.size(), std::vector<int>(text2.size(), -1));
-     return getCount(text1, text2, storage, 0, 0);
- 
+   return prev[0];
+                                          
     }
 };
