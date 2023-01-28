31798a4bee4328f800496e0572b63c5d80470656
Ankit D <a.singh725098@gmail.com>
2023-01-28 13:34:11 +0530

    Time: 15 ms (66.53%), Space: 6.4 MB (97.53%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -8,37 +8,38 @@ public:
     if (text2.size() > text1.size())
         swap(text1, text2);
 
-    std::vector<std::vector<int>> storage(text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+    std::vector<int> prev(text2.size() + 1, 0), curr(text2.size() + 1, 0);
 
-    for (int i = 0; i < storage[0].size(); i++)
-        storage[storage.size() - 1][i] = text2.size() - i;
-    for (int i = 0; i < storage.size(); i++)
-        storage[i][storage[0].size() - 1] = text1.size() - i;
+    for (int i = 0; i < prev.size(); i++)
+        prev[i] = text2.size() - i;
 
-    for (int i = storage.size() - 2; i >= 0; i--)
+    for (int i = text1.size() - 1; i >= 0; i--)
     {
-        for (int j = storage[0].size() - 2; j >= 0; j--)
+        curr[curr.size() - 1] = text1.size() - i;
+        for (int j = curr.size() - 2; j >= 0; j--)
         {
             int val1 = 1e5, val2 = 1e5, val3 = 1e5, val4 = 1e5;
             if (text1[i] == text2[j])
             {
-                val1 = storage[i + 1][j + 1];
+                val1 = prev[j + 1];
             }
             else
             {
                 // delete
-                val2 = 1 + storage[i + 1][j];
+                val2 = 1 + prev[j];
                 // replace
-                val3 = 1 + storage[i + 1][j + 1];
+                val3 = 1 + prev[j + 1];
                 // insert
-                val4 = 1 + storage[i][j + 1];
+                val4 = 1 + curr[j + 1];
             }
 
-            storage[i][j] = std::min({val1, val2, val3, val4});
+            curr[j] = std::min({val1, val2, val3, val4});
         }
+
+        prev = curr;
     }
 
-    return storage[0][0];
+    return curr[0];
 
     }
 };
