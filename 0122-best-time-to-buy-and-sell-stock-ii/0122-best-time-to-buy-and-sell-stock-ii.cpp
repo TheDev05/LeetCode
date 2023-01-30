4f85b5de20b9edc97afe298ed9c521e307e3bfee
Ankit D <a.singh725098@gmail.com>
2023-01-30 13:55:09 +0530

    Time: 7 ms (79.62%), Space: 13.1 MB (31.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     
-int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, bool buy, int index)
-{
-    if (index >= num.size())
-        return 0;
-
-    if (storage[index][buy] == -1)
-    {
-        int val1 = 0, val2 = 0, val3 = 0, val4 = 0;
-        if (buy)
-        {
-            val1 = getCount(num, storage, 0, index + 1) - num[index];
-            val2 = getCount(num, storage, buy, index + 1);
-        }
-        else
-        {
-            val3 = num[index] + getCount(num, storage, 1, index + 1);
-            val4 = getCount(num, storage, buy, index + 1);
-        }
-
-        storage[index][buy] = std::max({val1, val2, val3, val4});
-    }
-
-    return storage[index][buy];
-}
-
-    
     int maxProfit(vector<int>& num) {
         
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(2, -1));
-    return getCount(num, storage, 1, 0);
+    std::vector<int> prev(2, 0);
+    for (int i = num.size() - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            int val1 = 0, val2 = 0, val3 = 0, val4 = 0;
+            if (j)
+            {
+                val1 = prev[0] - num[i];
+                val2 = prev[j];
+            }
+            else
+            {
+                val3 = num[i] + prev[1];
+                val4 = prev[j];
+            }
 
+            prev[j] = std::max({val1, val2, val3, val4});
+        }
+    }
 
+    return prev[1];
+        
     }
 };
