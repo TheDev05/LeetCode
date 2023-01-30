4e042377e4a2516dceef575721808c8471339be6
Ankit D <a.singh725098@gmail.com>
2023-01-30 19:23:15 +0530

    Time: 800 ms (28.99%), Space: 207.1 MB (34.00%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,8 +1,10 @@
-class Solution {
-public:
-    
-    int maxProfit(vector<int>& num) {
-        
+class Solution
+{
+    public:
+
+        int maxProfit(vector<int> &num)
+        {
+
     std::vector<std::vector<std::vector<int>>> storage(num.size() + 1, std::vector<std::vector<int>>(2, std::vector<int>(3, 0)));
     for (int i = storage.size() - 2; i >= 0; i--)
     {
@@ -10,25 +12,29 @@ public:
         {
             for (int k = 0; k < 3; k++)
             {
-                int val1 = 0, val2 = 0, val3 = 0, val4 = 0;
                 if (j)
                 {
+                    int val1 = 0, val2 = 0;
                     if (k > 0)
                         val1 = storage[i + 1][0][k - 1] - num[i];
                     val2 = storage[i + 1][j][k];
+
+                    storage[i][j][k] = std::max(val1, val2);
                 }
                 else
                 {
-                    val3 = num[i] + storage[i + 1][1][k];
-                    val4 = storage[i + 1][j][k];
+                    int val1 = 0, val2 = 0;
+                    val1 = num[i] + storage[i + 1][1][k];
+                    val2 = storage[i + 1][j][k];
+
+                    storage[i][j][k] = std::max(val1, val2);
                 }
 
-                storage[i][j][k] = std::max({val1, val2, val3, val4});
+                // storage[i][j][k] = std::max({val1, val2, val3, val4});
             }
         }
     }
 
     return storage[0][1][2];
-
-    }
+        }
 };
