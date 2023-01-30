a7d4476f1622920a5b11383a51ccdb7152f455af
Ankit D <a.singh725098@gmail.com>
2023-01-30 13:22:06 +0530

    Time: 19 ms (18.25%), Space: 23.6 MB (6.50%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     
-int getCount(std::vector<int> &num, std::vector<int> &storage, int buy, int index)
+int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, bool buy, int index)
 {
     if (index >= num.size())
         return 0;
 
-    if (storage[index] == -1)
+    if (storage[index][buy] == -1)
     {
-        int val1 = 0, val2 = 0, val3 = 0;
-        if (buy >= 0 && num[index] >= buy)
+        int val1 = 0, val2 = 0, val3 = 0, val4 = 0;
+        if (buy)
         {
-            val1 = num[index] - buy;
+            val1 = getCount(num, storage, 0, index + 1) - num[index];
+            val2 = getCount(num, storage, buy, index + 1);
+        }
+        else
+        {
+            val3 = num[index] + getCount(num, storage, 1, index + 1);
+            val4 = getCount(num, storage, buy, index + 1);
         }
 
-        val3 = getCount(num, storage, num[index], index + 1);
-        val2 = getCount(num, storage, buy, index + 1);
-
-        storage[index] = std::max(val1 + val3, val2);
+        storage[index][buy] = std::max({val1, val2, val3, val4});
     }
 
-    return storage[index];
+    return storage[index][buy];
 }
+
     
     int maxProfit(vector<int>& num) {
         
-    std::vector<int> storage(num.size(), -1);
-    return getCount(num, storage, -1, 0);
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(2, -1));
+    return getCount(num, storage, 1, 0);
+
 
     }
 };
