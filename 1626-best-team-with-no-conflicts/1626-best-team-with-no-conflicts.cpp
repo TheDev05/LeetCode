bcb784ad9a151401063a5470942931e4e4efbc5f
Ankit D <a.singh725098@gmail.com>
2023-01-31 19:49:26 +0530

    Time: 501 ms (22.72%), Space: 20.2 MB (47.40%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    int getCount(std::vector<std::pair<int, int>> &num, std::vector<std::vector<int>> &storage, int temp, int index)
-{
-    if (index >= num.size())
-        return 0;
-
-    if (storage[index][temp] == -1)
-    {
-        int val1 = 0, val2 = 0;
-        if (temp == 0 || num[index].second >= num[temp - 1].second)
-            val1 = num[index].second + getCount(num, storage, index + 1, index + 1);
-        val2 = getCount(num, storage, temp, index + 1);
-
-        storage[index][temp] = std::max(val1, val2);
-    }
-
-    return storage[index][temp];
-}
-
-    
     int bestTeamScore(vector<int>& num1, vector<int>& num2) {
     std::vector<std::pair<int, int>> num;
     for (int i = 0; i < num1.size(); i++)
     {
         num.push_back({num2[i], num1[i]});
     }
-
+        
     sort(num.begin(), num.end());
+        
+    std::vector<int> prev(num.size() + 1, 0), curr(num.size() + 1, 0);
+    for (int i = num.size() - 1; i >= 0; i--)
+    {
+        for (int j = prev.size() - 2; j >= 0; j--)
+        {
+            int val1 = 0, val2 = 0;
+            if (j == 0 || num[i].second >= num[j - 1].second)
+                val1 = num[i].second + prev[i + 1];
+            val2 = prev[j];
 
-    std::vector<std::vector<int>> storage(num.size(),       std::vector<int>(num.size(), -1));
-    return getCount(num, storage, 0, 0);
+            curr[j] = std::max(val1, val2);
+        }
+
+        prev = curr;
+    }
+
+    return curr[0];
         
     }
 };
