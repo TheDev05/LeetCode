131dec9f3e5d76c9fe2ff370840959dd7cfebb04
Ankit D <a.singh725098@gmail.com>
2023-01-31 17:41:49 +0530

    Time: 1116 ms (17.76%), Space: 10.6 MB (55.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& num) {
-    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(num.size() + 1, 0));
-    for (int i = storage.size() - 2; i >= 0; i--)
+    std::vector<int> prev(num.size() + 1, 0);
+    for (int i = num.size() - 1; i >= 0; i--)
     {
-        for (int j = 0; j < num.size(); j++)
+        for (int j = 0; j < prev.size() - 1; j++)
         {
             int val1 = 0, val2 = 0;
             if (j == 0 || num[i] > num[j - 1])
-                val1 = 1 + storage[i + 1][i + 1];
-            val2 = storage[i + 1][j];
+                val1 = 1 + prev[i + 1];
+            val2 = prev[j];
 
-            storage[i][j] = std::max(val1, val2);
+            prev[j] = std::max(val1, val2);
         }
     }
 
-    return storage[0][0];
+    return prev[0];
+        
     }
 };
