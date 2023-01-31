479525a7d300628c7335c4ec5bfd000e111045c5
Ankit D <a.singh725098@gmail.com>
2023-01-31 15:47:34 +0530

    Time: 1670 ms (6.53%), Space: 291.1 MB (20.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-int getCount(std::vector<int> &num, std::vector<int> &storage, int index)
+int getCount(std::vector<int> &num, std::vector<std::vector<int>> &storage, int prevIndex, int index)
 {
     if (index >= num.size())
         return 0;
 
-    if (storage[index] == -1)
+    if (storage[index][prevIndex] == -1)
     {
-        int max = 0;
-        for (int j = index + 1; j < num.size(); j++)
-        {
-            if (num[j] > num[index])
-                max = std::max(max, getCount(num, storage, j));
-        }
+        int val1 = 0, val2 = 0;
+        if (prevIndex == 0 || num[index] > num[prevIndex - 1])
+            val1 = 1 + getCount(num, storage, index + 1, index + 1);
+        val2 = getCount(num, storage, prevIndex, index + 1);
 
-        storage[index] = 1 + max;
+        storage[index][prevIndex] = std::max(val1, val2);
     }
 
-    return storage[index];
+    return storage[index][prevIndex];
 }
-    
+
     int lengthOfLIS(vector<int>& num) {
         
-    int max = 0;
-    std::vector<int> storage(num.size(), -1);
-
-    for (int i = 0; i < num.size(); i++)
-    {
-        max = std::max(max, getCount(num, storage, i));
-    }
-
-    return max;
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(num.size(), -1));
+    return getCount(num, storage, 0, 0);
         
     }
 };
