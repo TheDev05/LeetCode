f7d8f53ff5116a2f6a25d97e028ae25e6109e0ff
Ankit D <a.singh725098@gmail.com>
2023-02-05 21:20:30 +0530

    Time: 8 ms (45.16%), Space: 11.9 MB (36.23%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-  
     int maxProfit(vector<int>& num) {
-         std::vector<std::vector<int>> prev(2, std::vector<int>(2, 0)), curr(2, std::vector<int>(2, 0));
-    for (int i = num.size() - 1; i >= 0; i--)
+    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(2, 0));
+    for (int i = storage.size() - 2; i >= 0; i--)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < storage[0].size(); j++)
         {
-            for (int k = 0; k < 2; k++)
+            if (j)
             {
-                if (j)
-                {
-                    int val1 = 0, val2 = 0;
-                    if (k)
-                        val1 = prev[0][0] - num[i];
-                    val2 = prev[j][1];
+                int val1 = 0, val2 = 0;
+                val1 = storage[i + 1][0] - num[i];
+                val2 = storage[i + 1][j];
 
-                    curr[j][k] = std::max(val1, val2);
-                }
-                else
-                {
-                    int val1 = 0, val2 = 0;
-                    val1 = num[i] + prev[1][0];
-                    val2 = prev[j][1];
+                storage[i][j] = std::max(val1, val2);
+            }
+            else
+            {
+                int val1 = num[i], val2 = 0;
+                if (i + 2 < storage.size())
+                    val1 += storage[i + 2][1];
+                val2 = storage[i + 1][j];
 
-                    curr[j][k] = std::max(val1, val2);
-                }
+                storage[i][j] = std::max(val1, val2);
             }
         }
-
-        prev = curr;
     }
 
-    return curr[1][1];
+    return storage[0][1];
+        
     }
 };
