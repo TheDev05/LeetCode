1809c96a91663870a759baba37883f91ce973f3b
Ankit D <a.singh725098@gmail.com>
2023-02-05 20:16:40 +0530

    Time: 880 ms (8.07%), Space: 154 MB (5.23%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int numSquares(int n) {
-        
     std::vector<int> num;
     for (int i = 1; i <= n; i++)
     {
-        double temp = sqrt(i);
-        if (ceil(temp) == floor(temp))
+        double val = sqrt(i);
+        if (ceil(val) == floor(val))
             num.push_back(i);
     }
 
-    std::vector<int> prev(n + 1, 1e7);
-    prev[0] = 0;
+    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(n + 1, 1e5));
+    storage[storage.size() - 1][0] = 0;
     
-    for (int i = num.size() - 1; i >= 0; i--)
+    for (int i = storage.size() - 2; i >= 0; i--)
     {
-        for (int j = 0; j < prev.size(); j++)
+        for (int j = 0; j < storage[0].size(); j++)
         {
-            int val1 = 1e7, val2 = 1e7;
+            int count1 = 1e5, count2 = 1e5;
             if (j - num[i] >= 0)
-                val1 = 1 + prev[j - num[i]];
-            val2 = prev[j];
+                count1 = 1 + storage[i][j - num[i]];
 
-            prev[j] = std::min(val1, val2);
+            count2 = storage[i + 1][j];
+
+            storage[i][j] = std::min(count1, count2);
         }
     }
 
-    return prev[n];
-        
+    return storage[0][n];
     }
 };
