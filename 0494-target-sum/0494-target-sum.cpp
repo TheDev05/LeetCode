5dab38bd652f8b1b67da96a36379cf649a86988c
Ankit D <a.singh725098@gmail.com>
2023-02-07 14:53:52 +0530

    Time: 445 ms (31.53%), Space: 62.4 MB (9.70%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-       
+int getCount(std::vector<int> &num, std::map<std::pair<int, int>, int> &storage, int k, int index)
+{
+    if (index >= num.size())
+        return (k == 0) ? 1 : 0;
+
+    if (storage.count({index, k}) == false)
+    {
+        int val1 = 0, val2 = 0;
+        val1 = getCount(num, storage, k + num[index], index + 1);
+        val2 = getCount(num, storage, k - num[index], index + 1);
+
+        storage[{index, k}] = (val1 + val2);
+    }
+
+    return storage[{index, k}];
+}
+    
     int findTargetSumWays(vector<int>& num, int k) {
-    int total = 0, sum = 0;
-    for (auto i : num)
-        total += i;
-        
-    if (k < -total || k > total)
-        return 0;
-
-    if ((total + k) % 2 == 0)
-    {
-        sum = (total + k) / 2;
-    }
-    else if ((total - k) % 2 == 0)
-    {
-        sum = (total - k) / 2;
-    }else return 0;
-
-    std::vector<int> prev(sum + 1, 0);
-    prev[0] = 1;
-
-    for (int i = num.size() - 1; i >= 0; i--)
-    {
-        for (int j = prev.size()-1; j >= 0; j--)
-        {
-            int val1 = 0, val2 = 0;
-            if (j - num[i] >= 0)
-                val1 = prev[j - num[i]];
-            val2 = prev[j];
-
-            prev[j] = val1 + val2;
-        }
-    }
-
-    return prev[sum];
+    std::map<std::pair<int, int>, int> storage;
+    return getCount(num, storage, k, 0);
         
     }
 };
