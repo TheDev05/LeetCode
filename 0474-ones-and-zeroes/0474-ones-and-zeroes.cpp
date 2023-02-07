ba9493882d058e62bc73cf6ced440a560068fd7c
Ankit D <a.singh725098@gmail.com>
2023-02-07 14:03:11 +0530

    Time: 645 ms (18.67%), Space: 102.9 MB (12.57%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,25 +1,7 @@
 class Solution {
 public:
-int getCount(std::vector<std::pair<int, int>> &num, std::vector<std::vector<std::vector<int>>> &storage, int zero, int one, int index)
-{
-    if (index >= num.size())
-        return 0;
-
-    if (storage[index][zero][one] == -1)
-    {
-        int val1 = 0, val2 = 0;
-        if (num[index].first <= zero && num[index].second <= one)
-            val1 = 1 + getCount(num, storage, zero - num[index].first, one - num[index].second, index + 1);
-        val2 = getCount(num, storage, zero, one, index + 1);
-
-        storage[index][zero][one] = std::max(val1, val2);
-    }
-
-    return storage[index][zero][one];
-}  
     int findMaxForm(vector<string>& num, int a, int b) {
-        
-    std::vector<std::pair<int, int>> data;
+           std::vector<std::pair<int, int>> data;
     for (auto i : num)
     {
         int zero = 0, one = 0;
@@ -34,11 +16,25 @@ int getCount(std::vector<std::pair<int, int>> &num, std::vector<std::vector<std:
         data.push_back({zero, one});
     }
 
-    // for (auto i : data)
-    //     std::cout << i.first << " " << i.second << '\n';
+    std::vector<std::vector<std::vector<int>>> storage(num.size() + 1, std::vector<std::vector<int>>(a + 1, std::vector<int>(b + 1, 0)));
+    // std::cout << getCount(data, storage, a, b, 0);
 
-    std::vector<std::vector<std::vector<int>>> storage(num.size(), std::vector<std::vector<int>>(a + 1, std::vector<int>(b + 1, -1)));
-    return getCount(data, storage, a, b, 0); 
-        
+    for (int i = storage.size() - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < a + 1; j++)
+        {
+            for (int k = 0; k < b + 1; k++)
+            {
+                int val1 = 0, val2 = 0;
+                if (data[i].first <= j && data[i].second <= k)
+                    val1 = 1 + storage[i + 1][j - data[i].first][k - data[i].second];
+                val2 = storage[i + 1][j][k];
+
+                storage[i][j][k] = std::max(val1, val2);
+            }
+        }
+    }
+
+    return storage[0][a][b]; 
     }
 };
