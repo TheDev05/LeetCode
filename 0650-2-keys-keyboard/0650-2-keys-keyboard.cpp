5e5949a7fcf513fc142d246bc10c292a08e65a48
Ankit D <a.singh725098@gmail.com>
2023-02-08 19:44:07 +0530

    Time: 11 ms (57.14%), Space: 6 MB (82.90%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:   
+int getCount(int val, int temp, int n)
+{
+    if (val == n)
+        return 0;
+
+    int val1 = 1e5, val2 = 1e5;
+
+    if (val + temp <= n)
+        val2 = 1 + getCount(val + temp, temp, n);
+    if (temp != val)
+        val1 = 1 + getCount(val, val, n);
+
+    return std::min(val1, val2);
+}
+
     int minSteps(int n) {
         
     if(n==1) return 0;
-    std::vector<std::vector<int>> storage(n + 1, std::vector<int>(n + 1, 0));
-    for (int i = storage.size() - 2; i >= 0; i--)
-    {
-        for (int j = storage[0].size() - 1; j >= 0; j--)
-        {
-            int val1 = 1e5, val2 = 1e5;
-
-            if (i + j <= n)
-                val2 = 1 + storage[i + j][j];
-            if (j != i)
-                val1 = 1 + storage[i][i];
-
-            storage[i][j] = std::min(val1, val2);
-        }
-    }
-
-    return 1 + storage[1][1];
+    return 1 + getCount(1, 1, n);
    
     }
 };
