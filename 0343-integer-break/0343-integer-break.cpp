628ccdc3630d5b847fbf423a3b88d37c135ef92b
Ankit D <a.singh725098@gmail.com>
2023-02-09 10:10:06 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (7.22%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-int getCount(std::vector<std::vector<int>> &storage, int n, int k, int index)
-{
-    if (n == 0 || index >= k)
-        return 1;
-
-    if (storage[index][n] == -1)
+   int integerBreak(int n) {
+    std::vector<std::vector<int>> storage(n + 1, std::vector<int>(n + 1, 1));
+    for (int i = storage.size() - 2; i >= 1; i--)
     {
-        int val1 = 0, val2 = 0;
-        if (n - index >= 0)
-            val1 = index * getCount(storage, n - index, k, index);
-        val2 = getCount(storage, n, k, index + 1);
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            int val1 = 0, val2 = 0;
+            if (j - i >= 0)
+                val1 = i * storage[i][j - i];
+            val2 = storage[i + 1][j];
 
-        storage[index][n] = std::max(val1, val2);
+            storage[i][j] = std::max(val1, val2);
+        }
     }
 
-    return storage[index][n];
-}
-
-    int integerBreak(int n) {
-    std::vector<std::vector<int>> storage(n + 1, std::vector<int>(n + 1, -1));
-    return getCount(storage, n, n, 1);
+    return storage[1][n];
         
     }
 };
