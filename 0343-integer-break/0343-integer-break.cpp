e57105437eb6be07b93c1010583359f1774044c1
Ankit D <a.singh725098@gmail.com>
2023-02-09 09:52:27 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (7.22%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-int getCount(int n, int k, int index)
+int getCount(std::vector<std::vector<int>> &storage, int n, int k, int index)
 {
     if (n == 0 || index >= k)
         return 1;
 
-    int val1 = 0, val2 = 0;
-    if (n - index >= 0)
-        val1 = index * getCount(n - index, k, index);
-    val2 = getCount(n, k, index + 1);
+    if (storage[index][n] == -1)
+    {
+        int val1 = 0, val2 = 0;
+        if (n - index >= 0)
+            val1 = index * getCount(storage, n - index, k, index);
+        val2 = getCount(storage, n, k, index + 1);
 
-    return std::max(val1, val2);
+        storage[index][n] = std::max(val1, val2);
+    }
+
+    return storage[index][n];
 }
 
     int integerBreak(int n) {
-    return getCount(n, n, 1);
+    std::vector<std::vector<int>> storage(n + 1, std::vector<int>(n + 1, -1));
+    return getCount(storage, n, n, 1);
         
     }
 };
