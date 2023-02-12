25ab4361eab403e421452855dbcb1cf4afa35528
Ankit D <a.singh725098@gmail.com>
2023-02-12 23:27:31 +0530

    Time: 6 ms (59.14%), Space: 13.6 MB (36.90%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,34 +11,46 @@
  */
 class Solution {
 public:
-int Preorder(TreeNode *root, std::map<int, std::map<int, std::multiset<int>>> &num, int i, int j)
+void Level(TreeNode *root, std::map<int, std::map<int, std::multiset<int>>> &num)
 {
     if (root == NULL)
-        return 0;
+        return;
 
-    num[i][j].insert(root->val);
-    Preorder(root->left, num, i - 1, j + 1);
-    Preorder(root->right, num, i + 1, j + 1);
+    std::queue<std::pair<TreeNode *, std::pair<int, int>>> temp;
+    temp.push({root, {0, 0}});
 
-    return 0;
+    while (temp.size())
+    {
+        int size = temp.size();
+        for (int i = 0; i < size; i++)
+        {
+            auto inox = temp.front();
+            temp.pop();
+
+            if (inox.first->left != NULL)
+                temp.push({inox.first->left, {inox.second.first - 1, inox.second.second + 1}});
+
+            if (inox.first->right != NULL)
+                temp.push({inox.first->right, {inox.second.first + 1, inox.second.second + 1}});
+
+            num[inox.second.first][inox.second.second].insert(inox.first->val);
+        }
+    }
 }
 
     vector<vector<int>> verticalTraversal(TreeNode* root) {
     std::map<int, std::map<int, std::multiset<int>>> num;
-    Preorder(root, num, 0, 0);
+    Level(root, num);
 
     std::vector<std::vector<int>> result;
     for (auto i : num)
     {
         std::vector<int> temp;
         for (auto j : i.second)
-        {
             temp.insert(temp.end(), j.second.begin(), j.second.end());
-        }
-        
         result.push_back(temp);
     }
-    
+        
     return result;
     }
 };
