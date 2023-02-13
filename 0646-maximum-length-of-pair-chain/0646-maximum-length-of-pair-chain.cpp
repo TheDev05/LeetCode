7c815d551ef9abeb11672add65d3329ee8e487df
Ankit D <a.singh725098@gmail.com>
2023-02-13 17:37:04 +0530

    Time: 213 ms (55.93%), Space: 22.8 MB (56.15%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& num) {
+        
+    int n=num.size();
+        
+    std::vector<int> storage(n, 1);
     std::sort(num.begin(), num.end());
 
-    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(num.size()+1, 0));
-    for (int i = storage.size() - 2; i >= 0; i--)
+    int result = 0;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < storage[0].size(); j++)
+        int max = 0;
+        for (int j = i - 1; j >= 0; j--)
         {
-            int val1 = 0, val2 = 0;
-            if (j == 0 || num[j - 1][1] < num[i][0])
-            val1 = 1 + storage[i + 1][i + 1];
-            val2 = storage[i + 1][j];
-
-            storage[i][j] = std::max(val1, val2);
+            if (num[j][1] < num[i][0])
+                max = std::max(max, storage[j]);
         }
+
+        storage[i] = 1 + max;
+        result = std::max(result, storage[i]);
     }
 
-    return storage[0][0];
+    return result;
     }
 };
