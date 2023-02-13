9e8cbd176504b6db2bec32cf65626b591a3d015b
Ankit D <a.singh725098@gmail.com>
2023-02-13 17:36:52 +0530

    Time: 938 ms (5.05%), Space: 159.2 MB (7.82%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-int getCount(std::vector<std::pair<int, int>> &num, std::vector<std::vector<int>> &storage, int prevIndex, int index)
-{
-    if (index >= num.size())
-        return 0;
-
-    if (storage[index][prevIndex] == -1)
-    {
-        int val1 = 0, val2 = 0;
-        if (prevIndex == 0 || num[prevIndex - 1].second < num[index].first)
-            val1 = 1 + getCount(num, storage, index + 1, index + 1);
-        val2 = getCount(num, storage, prevIndex, index + 1);
-
-        storage[index][prevIndex] = std::max(val1, val2);
-    }
-
-    return storage[index][prevIndex];
-}
-    
-    int findLongestChain(vector<vector<int>>& temp) {
-        
-    std::vector<std::pair<int, int>> num(temp.size());
-    for (int i = 0; i < temp.size(); i++)
-    {
-        num[i].first = temp[i][0];
-        num[i].second = temp[i][1];
-    }
-        
+    int findLongestChain(vector<vector<int>>& num) {
     std::sort(num.begin(), num.end());
 
-    std::vector<std::vector<int>>storage(num.size(), std::vector<int>(num.size(), -1));
-    return getCount(num, storage, 0, 0);      
+    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(num.size()+1, 0));
+    for (int i = storage.size() - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            int val1 = 0, val2 = 0;
+            if (j == 0 || num[j - 1][1] < num[i][0])
+            val1 = 1 + storage[i + 1][i + 1];
+            val2 = storage[i + 1][j];
+
+            storage[i][j] = std::max(val1, val2);
+        }
+    }
+
+    return storage[0][0];
     }
 };
