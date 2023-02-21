85fad209af718e64af2edcca37867e2f678f2c81
Ankit D <a.singh725098@gmail.com>
2023-02-21 17:37:05 +0530

    Time: 172 ms (43.41%), Space: 73.3 MB (5.15%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string text) {
-    std::string temp = text;
-    reverse(temp.begin(), temp.end());
-        
-    std::vector<int> prev(temp.size() + 1, 0), curr(temp.size() + 1, 0);
-    for (int i = text.size() - 1; i >= 0; i--)
+int traverse(std::string &text1, std::string &text2, std::vector<std::vector<int>> &storage, int index1, int index2)
+{
+    if (index1 >= text1.size() || index2 >= text2.size())
+        return 0;
+
+    if (storage[index1][index2] == -1)
     {
-        for (int j = prev.size() - 2; j >= 0; j--)
-        {
-            int val1 = 0, val2 = 0, val3 = 0;
-            if (text[i] == temp[j])
-                val1 = 1 + prev[j + 1];
-            else
-            {
-                val2 = prev[j];
-                val3 = curr[j + 1];
-            }
+        int val1 = 0, val2 = 0;
+        if (text1[index1] == text2[index2])
+            val1 = 1 + traverse(text1, text2, storage, index1 + 1, index2 + 1);
+        else
+            val2 = std::max(traverse(text1, text2, storage, index1, index2 + 1), traverse(text1, text2, storage, index1 + 1, index2));
 
-            curr[j] = std::max({val1, val2, val3});
-        }
-
-        prev = curr;
+        storage[index1][index2] = std::max(val1, val2);
     }
 
-    return curr[0];
+    return storage[index1][index2];
+}
+    
+    int longestPalindromeSubseq(string text1) {
+    std::string text2, result;
+
+    text2 = text1;
+    reverse(text2.begin(), text2.end());
+
+    std::vector<std::vector<int>> storage(text1.size(), std::vector<int>(text2.size(), -1));
+  return  traverse(text1, text2, storage, 0, 0);
         
     }
 };
