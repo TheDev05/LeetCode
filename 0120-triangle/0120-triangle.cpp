2d8ce88ad799559d9a9fdffd0757405423f4dbe5
Ankit D <a.singh725098@gmail.com>
2023-02-23 17:20:28 +0530

    Time: 8 ms (50.53%), Space: 9 MB (35.96%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
+int traverse(std::vector<std::vector<int>> &num, std::vector<std::vector<int>> &storage, int index, int col)
+{
+    if (index >= num.size())
+        return 0;
+
+    if (storage[index][col] == -1)
+    {
+        int val1 = 0, val2 = 0;
+        val1 = num[index][col] + traverse(num, storage, index + 1, col);
+        val2 = num[index][col] + traverse(num, storage, index + 1, col + 1);
+
+        storage[index][col] = min(val1, val2);
+    }
+
+    return storage[index][col];
+}
+    
     int minimumTotal(vector<vector<int>>& num) {
-        
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(num.size(), -1));
-    for (int i = 0; i < storage[storage.size() - 1].size(); i++)
-    {
-        storage[storage.size() - 1][i] = num[num.size() - 1][i];
-    }
-
-    for (int i = num.size() - 2; i >= 0; i--)
-    {
-        for (int j = i; j >= 0; j--)
-        {
-            int down = storage[i + 1][j];
-            int diagonal = storage[i + 1][j + 1];
-
-            storage[i][j] = std::min(down, diagonal) + num[i][j];
-        }
-    }
-
-    return storage[0][0];
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(num[num.size() - 1].size(), -1));
+    return traverse(num, storage, 0, 0);
         
     }
 };
