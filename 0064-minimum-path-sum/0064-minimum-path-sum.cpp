78f795cf22a670307f7e2a3e779ee67037abf20c
Ankit D <a.singh725098@gmail.com>
2023-02-23 18:03:24 +0530

    Time: 13 ms (35.39%), Space: 10.1 MB (34.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& num) {
-        
-    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(num[0].size(), -1));
-    storage[num.size() - 1][num[0].size() - 1] = num[num.size() - 1][num[0].size() - 1];
+int traverse(std::vector<std::vector<int>> &num, std::vector<std::vector<int>> &storage, int row, int col)
+{
+    if (row == num.size() - 1 && col == num[0].size() - 1)
+        return num[row][col];
+    if (row >= num.size() || col >= num[0].size())
+        return 1e5;
 
-    for (int i = num.size() - 1; i >= 0; i--)
+    if (storage[row][col] == -1)
     {
-        for (int j = num[0].size() - 1; j >= 0; j--)
-        {
-            if (i == num.size() - 1 && j == num[0].size() - 1)
-                continue;
-            int down = INT_MAX, right = INT_MAX;
+        int val1 = 1e5, val2 = 1e5;
+        val1 = num[row][col] + traverse(num, storage, row + 1, col);
+        val2 = num[row][col] + traverse(num, storage, row, col + 1);
 
-            if (i != num.size() - 1)
-                down = storage[i + 1][j];
-            if (j != num[0].size() - 1)
-                right = storage[i][j + 1];
-
-            storage[i][j] = std::min(right, down) + num[i][j];
-        }
+        storage[row][col] = std::min(val1, val2);
     }
 
-     return storage[0][0];
+    return storage[row][col];
+}
+    
+    int minPathSum(vector<vector<int>>& num) {
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(num[0].size(), -1));
+    return traverse(num, storage, 0, 0);       
     }
 };
