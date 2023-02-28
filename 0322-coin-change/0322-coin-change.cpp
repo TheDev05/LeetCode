7459e4046cce05b688c7f65dada56282c2ff678b
Ankit D <a.singh725098@gmail.com>
2023-02-28 10:56:52 +0530

    Time: 182 ms (18.06%), Space: 46 MB (18.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int coinChange(vector<int>& num, int k) {
-    std::vector<std::vector<int>> storage(num.size() + 1, std::vector<int>(k + 1, 1e5));
-    storage[storage.size() - 1][0] = 0;
-    
-    for (int i = storage.size() - 2; i >= 0; i--)
-    {
-        for (int k = 0; k < storage[0].size(); k++)
-        {
-            int val1 = 1e5, val2 = 1e5;
-            if (k - num[i] >= 0)
-                val1 = 1 + storage[i][k - num[i]];
-            val2 = storage[i + 1][k];
+int traverse(std::vector<int> &num, std::vector<std::vector<int>> &storage, int k, int index)
+{
+    if (k == 0)
+        return 0;
 
-            storage[i][k] = std::min(val1, val2);
-        }
+    if (index >= num.size())
+        return 1e5;
+
+    if (storage[index][k] == -1)
+    {
+        int val1 = 1e5, val2 = 1e5;
+        if (k - num[index] >= 0)
+            val1 = 1 + traverse(num, storage, k - num[index], index);
+        val2 = traverse(num, storage, k, index + 1);
+
+        storage[index][k] = std::min(val1, val2);
     }
 
-    return (storage[0][k] == 1e5) ? -1 : storage[0][k]; 
+    return storage[index][k];
+}
+    
+    int coinChange(vector<int>& num, int k) {
+    std::vector<std::vector<int>> storage(num.size(), std::vector<int>(k + 1, -1));
+    int val = traverse(num, storage, k, 0);
+
+    if(val==1e5) return -1;
+    else return val;  
+        
     }
 };
