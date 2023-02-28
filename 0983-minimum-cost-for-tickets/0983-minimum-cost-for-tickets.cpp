1961237c5ba2bc5386c5fed95205bc45c42d8a77
Ankit D <a.singh725098@gmail.com>
2023-02-28 19:33:50 +0530

    Time: 11 ms (16.46%), Space: 19.4 MB (5.95%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        
-    std::vector<int> prev(366, 0);
-    for (int i = days.size() - 1; i >= 0; i--)
-    {
-        for (int j = 365; j >= 0; j--)
-        {
-            int val1 = costs[0], val2 = costs[1], val3 = costs[2];
-            if (days[i] < j)
-                prev[j] = prev[j];
-            else
-            {
-                if (days[i] + 1 < 366)
-                    val1 += prev[days[i] + 1];
-                if (days[i] + 7 < 366)
-                    val2 += prev[days[i] + 7];
-                if (days[i] + 30 < 366)
-                    val3 += prev[days[i] + 30];
+int traverse(std::vector<int> &num1, std::vector<int> &num2, std::vector<std::vector<int>> &storage, int k, int index)
+{
+    if (index >= num1.size())
+        return 0;
+    
+    if(k > 365) return 0;
 
-                prev[j] = std::min({val1, val2, val3});
-            }
+    if (storage[index][k] == -1)
+    {
+        if (num1[index] < k)
+            storage[index][k] = traverse(num1, num2, storage, k, index + 1);
+        else
+        {
+            int val1 = 0, val2 = 0, val3 = 0;
+            val1 = num2[0] + traverse(num1, num2, storage, num1[index] + 1, index + 1);
+            val2 = num2[1] + traverse(num1, num2, storage, num1[index] + 7, index + 1);
+            val3 = num2[2] + traverse(num1, num2, storage, num1[index] + 30, index + 1);
+
+            storage[index][k] = std::min({val1, val2, val3});
         }
     }
 
-    return prev[0];
+    return storage[index][k];
+}
+    
+    int mincostTickets(vector<int>& num1, vector<int>& num2) {
+    std::vector<std::vector<int>> storage(num1.size(), std::vector<int>(365 + 1, -1));
+    return traverse(num1, num2, storage, 0, 0);        
     }
 };
