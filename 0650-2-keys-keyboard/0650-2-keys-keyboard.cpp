0cb5dd6771d32793cdf2aecd10da68949882a8ff
Ankit D <a.singh725098@gmail.com>
2023-03-01 11:43:06 +0530

    Time: 14 ms (59.88%), Space: 6.1 MB (62.44%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,24 +1,25 @@
 class Solution {
-public:   
-int getCount(int val, int temp, int n)
+public:
+int traverse(int sum, int copy, int target)
 {
-    if (val == n)
+    if (sum > target)
+        return 1e5;
+    if (sum == target)
         return 0;
 
     int val1 = 1e5, val2 = 1e5;
-
-    if (val + temp <= n)
-        val2 = 1 + getCount(val + temp, temp, n);
-    if (temp != val)
-        val1 = 1 + getCount(val, val, n);
+    if (copy != sum)
+        val1 = 1 + traverse(sum, sum, target);
+    val2 = 1 + traverse(sum + copy, copy, target);
 
     return std::min(val1, val2);
 }
-
+    
     int minSteps(int n) {
+    if (n == 1)
+        return 0;
         
-    if(n==1) return 0;
-    return 1 + getCount(1, 1, n);
-   
+    return 1 + traverse(1, 1, n);
+        
     }
 };
