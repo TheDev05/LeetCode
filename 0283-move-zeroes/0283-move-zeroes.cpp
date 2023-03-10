2dea11cf7eebee4d86e747e7c3725097aeee97a2
Ankit D <a.singh725098@gmail.com>
2023-03-11 00:08:53 +0530

    Time: 27 ms (51.52%), Space: 19.2 MB (86.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n=nums.size();
+        int i=-1;
+        
+        for(int j=0; j<n; j++){
+            
+            if(nums[j]!=0){
+                std::swap(nums[++i],nums[j]);
+            }
+        }
+    }
+    };
