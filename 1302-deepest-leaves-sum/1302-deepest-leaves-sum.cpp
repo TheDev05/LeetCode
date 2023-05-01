81124e02182e27b7f116ccb1b109717b5784df5b
Ankit D <a.singh725098@gmail.com>
2023-05-01 10:51:07 +0530

    Time: 122 ms (14.42%), Space: 59.9 MB (79.96%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,25 +11,23 @@
  */
 class Solution {
 public:
-
-// Using DFS
-int traverse(TreeNode *root, std::map<int, int> &data, int level)
+int traverse(TreeNode *root, std::map<int, int> &data, int count)
 {
     if (root == NULL)
         return 0;
 
-    data[level] += root->val;
+    data[count] += root->val;
 
-    traverse(root->left, data, level + 1);
-    traverse(root->right, data, level + 1);
+    traverse(root->left, data, count + 1);
+    traverse(root->right, data, count + 1);
 
     return 0;
 }
+    
     int deepestLeavesSum(TreeNode* root) {
     std::map<int, int> data;
-    traverse(root, data, 0);
+    traverse(root, data, 1);
 
-    return data.rbegin()->second;
-        
+    return (data.rbegin()->second);        
     }
 };
