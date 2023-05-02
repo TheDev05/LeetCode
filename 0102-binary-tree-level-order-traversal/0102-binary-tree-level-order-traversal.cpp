8dc392cdecbd195df307c00c72d795ed3ae7dfaa
Ankit D <a.singh725098@gmail.com>
2023-05-03 01:32:39 +0530

    Time: 4 ms (63.04%), Space: 12.6 MB (31.28%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,40 +11,39 @@
  */
 class Solution {
 public:
-void traverse(TreeNode *root, std::vector<std::vector<int>> &store)
+void traverse(TreeNode *root, std::vector<std::vector<int>> &num)
 {
     if (root == NULL)
         return;
 
-    std::deque<TreeNode *> inox;
-    inox.push_back(root);
+    std::queue<TreeNode *> inox;
+    inox.push(root);
 
     while (inox.size())
     {
-        std::vector<int> temp;
         int size = inox.size();
+        std::vector<int> atom;
 
         for (int i = 0; i < size; i++)
         {
-            TreeNode *atom = inox.front();
+            TreeNode *temp = inox.front();
+            inox.pop();
 
-            if (atom->left != NULL)
-                inox.push_back(atom->left);
-            if (atom->right != NULL)
-                inox.push_back(atom->right);
+            if (temp->left != NULL)
+                inox.push(temp->left);
+            if (temp->right != NULL)
+                inox.push(temp->right);
 
-            temp.push_back(atom->val);
-            inox.pop_front();
+            atom.push_back(temp->val);
         }
 
-        store.push_back(temp);
+        num.push_back(atom);
     }
-
-    return;
 }
 
     
     vector<vector<int>> levelOrder(TreeNode* root) {
+        
     std::vector<std::vector<int>> store;
     traverse(root, store);
     
