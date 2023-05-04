94133e831ba9b937125ab25da433e9b8e2d718ad
Ankit D <a.singh725098@gmail.com>
2023-05-05 00:24:19 +0530

    Time: 140 ms (63.00%), Space: 86.3 MB (56.79%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,22 +11,20 @@
  */
 class Solution {
 public:
-void traverse(TreeNode *root, std::map<int, int> &data, int &count)
+void traverse(TreeNode *root, int max, int &count)
 {
     if (root == NULL)
         return;
 
-    if (data.size() && data.rbegin()->first <= root->val)
+    if (root->val >= max)
         count++;
 
-    data[root->val]++;
+    int temp = max;
 
-    traverse(root->left, data, count);
-    traverse(root->right, data, count);
+    traverse(root->left, std::max(max, root->val), count);
+    traverse(root->right, std::max(max, root->val), count);
 
-    data[root->val]--;
-    if (data[root->val] == 0)
-        data.erase(root->val);
+    max = temp;
 
     return;
 }
@@ -34,8 +32,8 @@ void traverse(TreeNode *root, std::map<int, int> &data, int &count)
     int goodNodes(TreeNode* root) {
     std::map<int, int> data;
 
-    int count = 1;
-    traverse(root, data, count);
+    int count = 0;
+    traverse(root, root->val, count);
     
     return count;
     }
