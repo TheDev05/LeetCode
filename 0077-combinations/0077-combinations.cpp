f9ce5c821f52197c2500ca843bf715a633d16b9a
Ankit D <a.singh725098@gmail.com>
2023-05-16 11:21:53 +0530

    Time: 24 ms (81.23%), Space: 9 MB (76.40%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-int traverse(std::vector<int> &num, std::vector<int> &temp, std::vector<std::vector<int>> &result, int target, int index)
+int traverse(std::vector<int> &num, std::vector<int> &temp, std::vector<std::vector<int>> &store, int k, int index)
 {
-    if (index >= num.size())
+    if (temp.size() == k)
     {
-        if (temp.size() == target)
-            result.push_back(temp);
-
+        store.push_back(temp);
         return 0;
     }
 
-    temp.push_back(num[index]);
-    traverse(num, temp, result, target, index + 1);
+    if (index >= num.size())
+        return 0;
 
-    temp.pop_back();
-    traverse(num, temp, result, target, index + 1);
+    if (temp.size() < k)
+    {
+        temp.push_back(num[index]);
+        traverse(num, temp, store, k, index + 1);
+        temp.pop_back();
+    }
 
+    traverse(num, temp, store, k, index + 1);
     return 0;
 }
     
     vector<vector<int>> combine(int n, int k) {
-        
     std::vector<int> num(n);
     std::iota(num.begin(), num.end(), 1);
 
+    std::vector<std::vector<int>> store;
     std::vector<int> temp;
-    std::vector<std::vector<int>> result;
 
-    traverse(num, temp, result, k, 0);
-    return result;
+    traverse(num, temp, store, k, 0);
+    return store;
+        
     }
 };
