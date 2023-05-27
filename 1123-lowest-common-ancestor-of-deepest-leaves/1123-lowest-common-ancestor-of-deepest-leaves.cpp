6ed50efe5cea111e12dd5a49c998e9561dbdce6b
Ankit D <a.singh725098@gmail.com>
2023-05-27 16:51:07 +0530

    Time: 12 ms (82.42%), Space: 21 MB (85.90%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -11,70 +11,32 @@
  */
 class Solution {
 public:
-void bfs(TreeNode *root, std::set<int> &num)
+std::pair<TreeNode *, int> traverse(TreeNode *root, int height)
 {
-    if (root == NULL)
-        return;
+    if (root->left == NULL && root->right == NULL)
+        return {root, height};
 
-    std::queue<TreeNode *> qt;
-    qt.push(root);
+    std::pair<TreeNode *, int> temp1 = {NULL, 0}, temp2 = {NULL, 0};
 
-    while (qt.size())
-    {
-        int size = qt.size();
-        num.clear();
+    if (root->left != NULL)
+        temp1 = traverse(root->left, height + 1);
+    if (root->right != NULL)
+        temp2 = traverse(root->right, height + 1);
 
-        for (int i = 0; i < size; i++)
-        {
-            TreeNode *temp = qt.front();
-            qt.pop();
+    if (temp1.first != NULL && temp2.first != NULL && temp1.second == temp2.second)
+        return {root, temp1.second};
 
-            if (temp->left != NULL)
-                qt.push(temp->left);
-            if (temp->right != NULL)
-                qt.push(temp->right);
-
-            num.insert(temp->val);
-        }
-    }
-}
-
-TreeNode *traverse(TreeNode *root, std::set<int> &num)
-{
-    if (root == NULL)
-        return root;
-
-    TreeNode *temp1 = NULL;
-    TreeNode *temp2 = NULL;
-    TreeNode *temp3 = NULL;
-
-    temp1 = traverse(root->left, num);
-    temp2 = traverse(root->right, num);
-
-    if (num.count(root->val))
-        temp3 = root;
-
-    if (temp1 != NULL && temp2 != NULL || temp1 != NULL && temp3 != NULL || temp2 != NULL && temp3 != NULL)
-        return root;
-
-    if (temp1 != NULL)
+    if (temp1.second > temp2.second)
         return temp1;
-
-    if (temp2 != NULL)
+    else if (temp2.second > temp1.second)
         return temp2;
 
-    if (temp3 != NULL)
-        return temp3;
-
-    return NULL;
+    return {NULL, 0};
 }
     
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
 
-    std::set<int> num;
-
-    bfs(root, num);
-    return traverse(root, num);        
+    return traverse(root, 1).first;        
         
     }
 };
