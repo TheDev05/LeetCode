a74d270346ed19a1b3ce8309151f9ee43b3f7619
Ankit D <a.singh725098@gmail.com>
2023-06-16 09:05:21 +0530

    Added solution - LeetHub

---
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -0,0 +1,184 @@
+//{ Driver Code Starts
+// C++ program to find predecessor and successor in a BST
+#include "bits/stdc++.h"
+using namespace std;
+
+// BST Node
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+/* BST Node
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+*/
+
+// This function finds predecessor and successor of key in BST.
+// It sets pre and suc as predecessor and successor respectively
+class Solution
+{
+    public:
+Node *left(Node *root, Node *&pre, int key, int &min)
+{
+    if (root == NULL)
+        return NULL;
+
+    if (root->key < key)
+    {
+        if (root->key > min)
+        {
+            min = root->key;
+            pre = root;
+        }
+
+        left(root->right, pre, key, min);
+    }
+    else
+        left(root->left, pre, key, min);
+
+    return NULL;
+}
+
+Node *right(Node *root, Node *&suc, int key, int &max)
+{
+    if (root == NULL)
+        return NULL;
+
+    if (root->key > key)
+    {
+        if (root->key < max)
+        {
+            max = root->key;
+            suc = root;
+        }
+
+        right(root->left, suc, key, max);
+    }
+    else
+        right(root->right, suc, key, max);
+
+    return NULL;
+}
+
+    void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+    {
+        // Your code goes here
+    int min = INT_MIN, max = INT_MAX;
+
+    left(root, pre, key, min);
+    right(root, suc, key, max);
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+// Driver program to test above functions
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, s);
+        int k = stoi(s);
+        Node *pre=NULL,*succ=NULL;
+        Solution ob;
+        ob.findPreSuc(root,pre,succ,k);
+        (pre!=NULL)?cout<<pre->key:cout<<-1;
+        cout<<" ";
+        (succ!=NULL)?cout<<succ->key:cout<<-1;
+        cout<<endl;
+        // inOrderTraversal(root);
+   }
+   return 0;
+}
+// } Driver Code Ends
