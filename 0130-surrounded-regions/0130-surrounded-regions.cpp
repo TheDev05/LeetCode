fe9520ffe974827d23c80e9f9265a08b5769ed78
Ankit D <a.singh725098@gmail.com>
2023-08-11 00:06:50 +0530

    Time: 11 ms (81.50%), Space: 12.2 MB (12.22%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& num) {
+        
+    int n = num.size(), m = num[0].size();
+        
+    std::vector<std::vector<char>> vis(n, std::vector<char>(m, 0));
+    std::vector<std::vector<char>> result(n, std::vector<char>(m, 'X'));
+
+    int r[4] = {0, 0, -1, 1};
+    int c[4] = {-1, 1, 0, 0};
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (vis[i][j] == 0 && num[i][j] == 'O')
+            {
+                std::queue<std::pair<int, int>> test;
+                std::vector<std::pair<int, int>> local;
+
+                test.push({i, j});
+                vis[i][j] = 1;
+
+                bool ok = true;
+                while (test.size())
+                {
+                    int row = test.front().first;
+                    int col = test.front().second;
+
+                    test.pop();
+                    local.push_back({row, col});
+
+                    for (int k = 0; k < 4; k++)
+                    {
+                        int trow = row + r[k];
+                        int tcol = col + c[k];
+
+                        if (trow < 0 || trow >= n || tcol < 0 || tcol >= m)
+                            ok = false;
+
+                        if (trow >= 0 && trow < n && tcol >= 0 && tcol < m)
+                        {
+                            if (vis[trow][tcol] == 0 && num[trow][tcol] == 'O')
+                            {
+                                vis[trow][tcol] = 1;
+                                test.push({trow, tcol});
+                            }
+                        }
+                    }
+                }
+
+                if (ok)
+                    for (auto p : local)
+                        num[p.first][p.second] = 'X';
+            }
+        }
+    }        
+    }
+};
