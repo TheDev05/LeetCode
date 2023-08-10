4aa82d7dbcc9a12e9dc5fd1bb23d131ffde367b9
Ankit D <a.singh725098@gmail.com>
2023-08-11 01:38:04 +0530

    Time: 56 ms (91.43%), Space: 27.7 MB (70.76%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,57 +1,54 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& num) {
-    
-    std::vector<std::vector<int>> vis(num.size(), std::vector<int>(num[0].size(), 0));
-
-    int r[4] = {0, 0, -1, 1};
-    int c[4] = {1, -1, 0, 0};
-
-    int result = 0;
-    for (int i = 0; i < num.size(); i++)
+        
+    int n =num.size(), m = num[0].size();
+    std::queue<std::pair<int, int>> test;
+        
+    std::vector<std::vector<int>> vis(n, std::vector<int>(m, 0));
+        
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < num[0].size(); j++)
+        for (int j = 0; j < m; j++)
         {
-            if (vis[i][j] == 0 && num[i][j])
+            if ((i == 0 || i == n - 1 || j == 0 || j == m - 1) && num[i][j])
+                {test.push({i, j}); vis[i][j]=1;}
+        }
+    }
+    
+    int r[4] = {-1, 1, 0, 0};
+    int c[4] = {0, 0, -1, 1};
+        
+    while (test.size())
+    {
+        int row = test.front().first;
+        int col = test.front().second;
+
+        test.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int trow = row + r[i];
+            int tcol = col + c[i];
+
+            if (trow >= 0 && trow < n && tcol >= 0 && tcol < m && num[trow][tcol] && vis[trow][tcol] == 0)
             {
-                std::queue<std::pair<int, int>> store;
-
-                store.push({i, j});
-                vis[i][j] = 1;
-
-                bool ok = true;
-                int count = 0;
-
-                while (store.size())
-                {
-                    int row = store.front().first;
-                    int col = store.front().second;
-
-                    store.pop();
-
-                    if (row == 0 || col == 0 || row == num.size() - 1 || col == num[0].size() - 1)
-                        ok = false;
-                    count++;
-
-                    for (int p = 0; p < 4; p++)
-                    {
-                        int nrow = row + r[p];
-                        int ncol = col + c[p];
-
-                        if (nrow >= 0 && ncol >= 0 && nrow < num.size() && ncol < num[0].size() && num[nrow][ncol] && vis[nrow][ncol] == 0)
-                        {
-                            vis[nrow][ncol] = 1;
-                            store.push({nrow, ncol});
-                        }
-                    }
-                }
-
-                if (ok)
-                    result += count;
+                vis[trow][tcol] = 1;
+                test.push({trow, tcol});
             }
         }
     }
 
-    return result;        
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (vis[i][j] == 0 && num[i][j])
+                count++;
+        }
+    }
+
+    return count;        
     }
 };
