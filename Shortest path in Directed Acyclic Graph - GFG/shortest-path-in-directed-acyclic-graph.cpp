6141a951f185ac73bb076ed0a826d58f33ca0676
Ankit D <a.singh725098@gmail.com>
2023-08-16 11:15:57 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -9,19 +9,6 @@ using namespace std;
 class Solution {
   public:
   
-      void topoSort(int node, vector < pair < int, int >> adj[],
-      int vis[], stack < int > & st) {
-      //This is the function to implement Topological sort. 
-      vis[node] = 1;
-      for (auto it: adj[node]) {
-        int v = it.first;
-        if (!vis[v]) {
-          topoSort(v, adj, vis, st);
-        }
-      }
-      st.push(node);
-    }
-
      vector<int> shortestPath(int n,int m, vector<vector<int>>& num){
          
         // code here
@@ -33,39 +20,44 @@ class Solution {
         store[num[i][0]].push_back({num[i][1], num[i][2]});
     }
 
-    // 3 0 2 1
-    
-    int vis[n]={0};
-    
-    stack < int > st;
-      for (int i = 0; i < n; i++) {
-        if (!vis[i]) {
-          topoSort(i, store, vis, st);
-        }
-      }
-   
-     std::vector<int> res;   
-     while(st.size())
-     {
-         res.push_back(st.top());
-         st.pop();
-     }
+    for (auto i : store)
+    {
+        for (auto j : i)
+            indeg[j.first]++;
+    }
 
-    // for (auto i : res)
-    //     std::cout << i << " ";
+    std::queue<int> inox;
+    for (int i = 0; i < n; i++)
+    {
+        if (indeg[i] == 0)
+            inox.push(i);
+    }
+
+    std::vector<int> res;
+    while (inox.size())
+    {
+        int local = inox.front();
+        inox.pop();
+
+        res.push_back(local);
+
+        for (auto i : store[local])
+        {
+            indeg[i.first]--;
+            if (indeg[i.first] == 0)
+                inox.push(i.first);
+        }
+    }
 
     std::vector<int> dist(n, 1e6);
     dist[0] = 0;
 
     for (int i = 0; i < n; i++)
     {
-        int node = res[i];
-        
-        for (auto j: store[node]) {
-        dist[j.first] = std::min(dist[j.first], dist[node] + j.second);
-
+        for (auto j : store[res[i]])
+        {
+            dist[j.first] = std::min(dist[j.first], dist[res[i]] + j.second);
         }
-      
     }
 
     for (auto &i : dist)
