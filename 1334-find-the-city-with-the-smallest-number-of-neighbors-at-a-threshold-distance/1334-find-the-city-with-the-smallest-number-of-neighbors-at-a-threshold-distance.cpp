6aa50c2d546c43b7fb1f0b1083d8f3e79a77d463
Ankit D <a.singh725098@gmail.com>
2023-08-20 00:15:48 +0530

    Time: 2838 ms (5.01%), Space: 235.5 MB (5.13%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,26 +1,58 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& num, int lim) {
-        
-    std::vector<std::vector<int>> dist(n, std::vector<int>(n, 1e7));
-    for (int i = 0; i < num.size(); i++)
-    {
-        dist[num[i][0]][num[i][1]] = num[i][2];
-        dist[num[i][1]][num[i][0]] = num[i][2];
-    }
+int dijkstra(int src, int target, int n, std::vector<std::pair<int, int>> num[])
+{
+    std::vector<int> dist(n, 1e7);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, greater<std::pair<int, int>>> inox;
 
-    for (int v = 0; v < n; v++)
+    inox.push({0, src});
+    dist[src] = 0;
+
+    while (inox.size())
     {
-        for (int i = 0; i < n; i++)
+        auto it = inox.top();
+        inox.pop();
+
+        int node = it.second;
+        int wt = it.first;
+
+        for (auto i : num[node])
         {
-            for (int j = 0; j < n; j++)
+            int adjNode = i.first;
+            int adjWt = i.second;
+
+            if (wt + adjWt < dist[adjNode])
             {
-                dist[i][j] = std::min(dist[i][j], dist[i][v] + dist[v][j]);
-                if(i == j) dist[i][j] = 0;
+                dist[adjNode] = wt + adjWt;
+                inox.push({dist[adjNode], adjNode});
             }
         }
     }
 
+    return dist[target];
+}
+    
+    int findTheCity(int n, vector<vector<int>>& adj, int lim) {
+        
+    std::vector<std::pair<int, int>> num[n];
+    for (int i = 0; i < adj.size(); i++)
+    {
+        num[adj[i][0]].push_back({adj[i][1], adj[i][2]});
+        num[adj[i][1]].push_back({adj[i][0], adj[i][2]});
+    }
+
+    std::vector<std::vector<int>> dist(n, std::vector<int>(n, 0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                dist[i][j] = 0;
+            else
+                dist[i][j] = dijkstra(i, j, n, num);
+        }
+    }
+        
     std::vector<int> inox(n, 0);
 
     int min = INT_MAX;
