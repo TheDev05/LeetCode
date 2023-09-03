61cdcaa86a24979dd188c53a2f5ef860c4e39af2
Ankit D <a.singh725098@gmail.com>
2023-09-04 01:33:11 +0530

    Time: 150 ms (17.65%), Space: 52.7 MB (11.90%) - LeetHub

---
+++ 0851-loud-and-rich/0851-loud-and-rich.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+void traverse(std::vector<int> adj[], int index, int &resIndex, std::vector<int> &quiet, std::vector<int> &vis, int &val)
+{
+    vis[index] = 1;
+    if(quiet[index] < val)
+    {
+        val = quiet[index];
+        resIndex = index;
+    }
+
+    for (auto i : adj[index])
+    {
+        if (vis[i] == 0)
+            traverse(adj, i, resIndex, quiet, vis, val);
+    }
+}
+
+    vector<int> loudAndRich(vector<vector<int>>& num, vector<int>& quiet) {
+    int n = quiet.size();
+        
+    std::vector<int> adj[n];
+    for (int i = 0; i < num.size(); i++)
+    {
+        adj[num[i][1]].push_back(num[i][0]);
+    }
+
+    std::vector<int> res, vis(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        int val = INT_MAX, resIndex = i;
+        std::vector<int> vis(n, 0);
+
+        traverse(adj, i, resIndex, quiet, vis, val);
+        res.push_back(resIndex);
+    }  
+    
+    return res;
+    }
+};
