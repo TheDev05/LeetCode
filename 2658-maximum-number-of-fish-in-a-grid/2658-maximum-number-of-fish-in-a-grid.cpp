79f2b27fa907788540040aecb789173c2dbd597f
Ankit D <a.singh725098@gmail.com>
2023-09-12 19:01:25 +0530

    Time: 85 ms (33.23%), Space: 95.3 MB (30.94%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,51 +1,97 @@
 class Solution {
 public:
+
+class dsu
+{
+public:
+    std::vector<std::vector<int>> sum;
+    std::vector<std::vector<std::pair<int, int>>> parent;
+
+    dsu(int n, int m)
+    {
+        sum.resize(n + 1, std::vector<int>(m + 1, 0));
+        parent.resize(n + 1, std::vector<std::pair<int, int>>(m + 1, {0, 0}));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                sum[i][j] = 0;
+                parent[i][j] = {i, j};
+            }
+        }
+    }
+
+    void setSum(int val, int row, int col)
+    {
+        sum[row][col] = val;
+    }
+
+    std::pair<int, int> getParent(int row, int col)
+    {
+        if (parent[row][col].first == row && parent[row][col].second == col)
+            return {row, col};
+
+        return parent[row][col] = getParent(parent[row][col].first, parent[row][col].second);
+    }
+
+    void setSize(int row1, int col1, int row2, int col2)
+    {
+        auto parent1u = parent[row1][col1];
+        auto parent2u = parent[row2][col2];
+
+        if (sum[parent1u.first][parent1u.second] > sum[parent2u.first][parent2u.second])
+            std::swap(parent1u, parent2u);
+
+        parent[parent1u.first][parent1u.second] = {parent2u.first, parent2u.second};
+        sum[parent2u.first][parent2u.second] += sum[parent1u.first][parent1u.second];
+    }
+};
+
     int findMaxFish(vector<vector<int>>& num) {
-        
+    
     int n = num.size(), m = num[0].size();
-    std::vector<std::vector<int>> vis(n, std::vector<int>(m, 0));
+    dsu ds(n, m);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            ds.setSum(num[i][j], i, j);
+        }
+    }
 
     int r[4] = {0, 0, -1, 1};
     int c[4] = {1, -1, 0, 0};
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int row = i;
+            int col = j;
+
+            if (num[row][col])
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    int trow = row + r[k];
+                    int tcol = col + c[k];
+
+                    if (trow >= 0 && trow < n && tcol >= 0 && tcol < m &&
+                        ds.getParent(row, col) != ds.getParent(trow, tcol) && num[trow][tcol])
+                        ds.setSize(row, col, trow, tcol);
+                }
+            }
+        }
+    }
 
     int max = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            if (vis[i][j] == 0 && num[i][j])
-            {
-                std::queue<std::pair<int, int>> inox;
-
-                inox.push({i, j});
-                vis[i][j] = 1;
-
-                int sum = 0;
-                while (inox.size())
-                {
-                    auto it = inox.front();
-                    inox.pop();
-
-                    int row = it.first;
-                    int col = it.second;
-
-                    sum += num[row][col];
-
-                    for (int k = 0; k < 4; k++)
-                    {
-                        int trow = row + r[k];
-                        int tcol = col + c[k];
-
-                        if (trow >= 0 && trow < n && tcol >= 0 && tcol < m && vis[trow][tcol] == 0 && num[trow][tcol])
-                        {
-                            vis[trow][tcol] = 1;
-                            inox.push({trow, tcol});
-                        }
-                    }
-                }
-
-                max = std::max(max, sum);
-            }
+            max = std::max(max, ds.sum[i][j]);
         }
     }
 
