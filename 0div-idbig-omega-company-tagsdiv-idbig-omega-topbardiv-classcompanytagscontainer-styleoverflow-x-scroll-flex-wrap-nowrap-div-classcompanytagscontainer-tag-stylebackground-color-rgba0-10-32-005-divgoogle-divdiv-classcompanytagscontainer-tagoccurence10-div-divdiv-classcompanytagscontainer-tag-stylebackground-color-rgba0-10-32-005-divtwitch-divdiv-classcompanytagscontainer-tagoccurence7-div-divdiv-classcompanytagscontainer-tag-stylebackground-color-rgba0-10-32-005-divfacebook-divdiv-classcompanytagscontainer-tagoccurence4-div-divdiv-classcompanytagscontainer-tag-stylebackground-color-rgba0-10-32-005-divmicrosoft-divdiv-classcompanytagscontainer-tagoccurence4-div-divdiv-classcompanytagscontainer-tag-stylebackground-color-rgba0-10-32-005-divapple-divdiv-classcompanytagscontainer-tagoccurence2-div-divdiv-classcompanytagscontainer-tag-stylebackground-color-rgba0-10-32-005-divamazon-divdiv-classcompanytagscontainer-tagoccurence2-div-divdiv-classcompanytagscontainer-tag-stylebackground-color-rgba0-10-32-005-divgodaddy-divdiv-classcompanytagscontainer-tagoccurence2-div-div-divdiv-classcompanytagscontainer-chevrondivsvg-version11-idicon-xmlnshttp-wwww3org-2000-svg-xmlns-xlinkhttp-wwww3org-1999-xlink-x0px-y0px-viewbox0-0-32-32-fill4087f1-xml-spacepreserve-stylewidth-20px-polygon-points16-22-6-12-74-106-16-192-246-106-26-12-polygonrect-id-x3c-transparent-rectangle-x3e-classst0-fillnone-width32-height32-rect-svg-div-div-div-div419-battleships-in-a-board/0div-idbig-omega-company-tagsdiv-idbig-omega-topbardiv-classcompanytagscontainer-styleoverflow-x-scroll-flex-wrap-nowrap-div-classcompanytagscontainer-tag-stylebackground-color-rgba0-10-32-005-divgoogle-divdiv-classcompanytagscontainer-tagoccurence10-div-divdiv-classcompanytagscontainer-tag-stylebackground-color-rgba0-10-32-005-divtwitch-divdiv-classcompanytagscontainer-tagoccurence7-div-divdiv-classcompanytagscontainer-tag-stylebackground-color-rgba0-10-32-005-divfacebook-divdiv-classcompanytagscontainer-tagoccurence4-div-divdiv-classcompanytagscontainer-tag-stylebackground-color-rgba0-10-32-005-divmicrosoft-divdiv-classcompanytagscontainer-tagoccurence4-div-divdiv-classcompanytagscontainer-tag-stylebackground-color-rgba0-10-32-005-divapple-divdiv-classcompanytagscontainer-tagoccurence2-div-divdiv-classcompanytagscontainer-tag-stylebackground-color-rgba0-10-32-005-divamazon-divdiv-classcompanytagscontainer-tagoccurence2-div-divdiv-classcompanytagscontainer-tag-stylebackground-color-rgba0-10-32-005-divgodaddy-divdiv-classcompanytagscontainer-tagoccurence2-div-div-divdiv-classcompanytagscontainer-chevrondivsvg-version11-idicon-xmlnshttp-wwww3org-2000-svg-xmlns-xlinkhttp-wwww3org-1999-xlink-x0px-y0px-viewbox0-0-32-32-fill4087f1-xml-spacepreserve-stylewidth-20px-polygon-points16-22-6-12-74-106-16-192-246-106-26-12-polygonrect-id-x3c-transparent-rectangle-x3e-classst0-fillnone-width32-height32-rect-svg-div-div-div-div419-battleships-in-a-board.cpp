a36105f82942db2e34287d3fb998f5bb3388cf67
Ankit D <a.singh725098@gmail.com>
2023-10-09 23:00:33 +0530

    Time: 8 ms (42.54%), Space: 9.2 MB (8.23%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitch-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgodaddy-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div419-battleships-in-a-board/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitch-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgodaddy-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div419-battleships-in-a-board.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+class dsu
+{
+public:
+    std::vector<int> _size, parent;
+
+    dsu(int n)
+    {
+        _size.resize(n, 0);
+        parent.resize(n, 0);
+
+        for (int i = 0; i < _size.size(); i++)
+        {
+            _size[i] = 1;
+            parent[i] = i;
+        }
+    }
+
+    int getParent(int node)
+    {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = getParent(parent[node]);
+    }
+
+    void setUnion(int x, int y)
+    {
+        int parentDx = parent[x];
+        int parentDy = parent[y];
+
+        if (_size[parentDx] > _size[parentDy])
+            swap(x, y);
+
+        _size[parent[y]] += _size[parent[x]];
+        parent[x] = parent[y];
+    }
+};
+    
+    int countBattleships(vector<vector<char>>& num) {
+        
+    int n = num.size(), m = num[0].size();
+    std::vector<std::vector<int>> vis(n, std::vector<int>(m, 0));
+
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
+
+    dsu ds(n * m);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (num[i][j] == 'X')
+            {
+                vis[i][j] = 1;
+
+                for (int k = 0; k < 4; k++)
+                {
+                    int row = dx[k] + i;
+                    int col = dy[k] + j;
+
+                    if (row >= 0 && row < n && col >= 0 && col < m && num[row][col] == 'X' && vis[row][col] == 0)
+                    {
+                        int temp1 = (((i + 1) - 1) * m) + j;
+                        int temp2 = (((row + 1) - 1) * m) + col;
+
+                        if (ds.getParent(temp1) != ds.getParent(temp2))
+                            ds.setUnion(temp1, temp2);
+
+                        vis[row][col] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    std::set<int> count;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (vis[i][j])
+            {
+                int temp = (((i + 1) - 1) * m) + j;
+                count.insert(ds.parent[temp]);
+            }
+        }
+    }
+
+    return count.size();        
+    }
+};
