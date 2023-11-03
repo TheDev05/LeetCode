5c031a59c802c7cadb6ddbd447d44efc37082dac
Ankit D <a.singh725098@gmail.com>
2023-11-03 16:04:36 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (22.92%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& num, int n) {
         
-        std::vector<std::string>res;
-        int temp=0;
-        
-        for(int i=0;i<num.size();i++)
-        {
-            int count=abs(temp-num[i]);
-            for(int j=0;j<count-1;j++) res.push_back("Push"), res.push_back("Pop");
-            res.push_back("Push");
-            temp=num[i];
-        }
+    std::vector<std::string> res;
+    int temp = 0;
+
+    for (int i = 0; i < num.size(); i++)
+    {
+        int count = abs(temp - num[i]);
+
+        for (int j = 0; j < count - 1; j++)
+            res.push_back("Push"), res.push_back("Pop");
+
+        res.push_back("Push");
+        temp = num[i];
+    }
         
         return res;
     }
