6a29ede9b5e75631e9df2b59249b771de1dd94c8
Ankit D <a.singh725098@gmail.com>
2023-11-09 22:46:19 +0530

    Time: 3 ms (66.45%), Space: 12.6 MB (19.41%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& num) {
+        
+    int idx = 0;
+    for (int i = num.size() - 1; i >= 0; i--)
+    {
+        int delta = INT_MAX, index = i;
+        for (int j = i; j < num.size(); j++)
+        {
+            if (num[i] < num[j] && abs(num[i] - num[j]) < delta)
+            {
+                delta = abs(num[i] - num[j]);
+                index = j;
+            }
+        }
+
+        if (delta != INT_MAX)
+        {
+            std::swap(num[i], num[index]);
+            idx = i + 1;
+
+            break;
+        }
+    }
+
+    sort(num.begin()+idx, num.end());
+    }
+};
