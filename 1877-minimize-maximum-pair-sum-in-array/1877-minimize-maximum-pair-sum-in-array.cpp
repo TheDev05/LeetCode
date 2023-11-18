080eba7861784c6497a000c502ead5ff92f2d4bd
Ankit D <a.singh725098@gmail.com>
2023-11-18 09:18:01 +0530

    Time: 227 ms (5.47%), Space: 96.8 MB (18.57%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -5,10 +5,10 @@ public:
     ranges::sort(num);
     int max=0;
         
-    [&](){
+
         for(int i=0;i <num.size()/2;i++) 
             max = std::max(max, num[i]+num[num.size()-1-i]);
-    }();
+
         
     return max;
     }
