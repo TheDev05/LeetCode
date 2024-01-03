20adffbf8eb1f3f7e06e43f2231c5987f77b0def
Ankit D <a.singh725098@gmail.com>
2024-01-03 21:28:48 +0530

    Time: 88 ms (65.84%), Space: 23.8 MB (41.38%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,36 +1,22 @@
-/*
- * @lc app=leetcode id=2125 lang=cpp
- *
- * [2125] Number of Laser Beams in a Bank
- */
-
-// @lc code=start
-class Solution
-{
+class Solution {
 public:
-    int numberOfBeams(vector<string> &bank)
+    int numberOfBeams(vector<string>& num) {
+    std::vector<int> data;
+    for (int i = 0; i < num.size(); i++)
     {
+        int temp = count(num[i].begin(), num[i].end(), '1');
+        if (temp)
+            data.push_back(temp);
+    }
 
-        vector<int> num;
-        for (int i = 0; i < bank.size(); i++)
-        {
-            int val = count(bank[i].begin(), bank[i].end(), '1');
+    if(data.size() <= 1) return 0;
 
-            if (val > 0)
-            {
-                num.push_back(val);
-            }
-        }
-        
-        if(num.size()<2) return(0);
+    int sum = 0;
+    for (int i = 0; i < data.size() - 1; i++)
+    {
+        sum += (data[i] * data[i + 1]);
+    }
 
-        int sum = 0;
-        for (int i = 0; i < num.size() - 1; i++)
-        {
-            sum += (num[i] * num[i + 1]);
-        }
-
-        return(sum);
+    return sum;        
     }
 };
-// @lc code=end
