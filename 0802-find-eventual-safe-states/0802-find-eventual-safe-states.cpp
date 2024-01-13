0c6b91598d84b665e2f2bb94f8944b236d8ad7aa
Ankit D <a.singh725098@gmail.com>
2024-01-13 16:57:18 +0530

    Time: 135 ms (69.41%), Space: 52.5 MB (61.07%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,51 +1,46 @@
 class Solution {
 public:
+    int traverse(std::vector<int> adj[], std::vector<int> &vis, int index)
+    {
+        vis[index] = 1;
+        int count = 0;
+
+        for (auto i : adj[index])
+        {
+            if (vis[i] == -1)
+                count += traverse(adj, vis, i);
+            else
+                count += vis[i];
+        }
+
+        count = count ? 1 : 0;
+        vis[index] = count;
+
+        return count;
+    }
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& num) {
     int n = num.size();
-
-    std::vector<int> store[n];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < num[i].size(); j++)
-        {
-            store[num[i][j]].push_back(i);
-        }
-    }
-
-    std::vector<int> indeg(n, 0);
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < store[i].size(); j++)
-        {
-            indeg[store[i][j]]++;
-        }
-    }
-
-    std::queue<int> inox;
-    for (int i = 0; i < n; i++)
-    {
-        if (indeg[i] == 0)
-            inox.push(i);
-    }
-
-    std::vector<int> result;
-    while (inox.size())
-    {
-        int local = inox.front();
-        inox.pop();
-
-        result.push_back(local);
-
-        for (auto i : store[local])
-        {
-            indeg[i]--;
-            if (indeg[i] == 0)
-                inox.push(i);
-        }
-    }
-     
-    sort(result.begin(), result.end());
-    return result;
         
+    std::vector<int> adj[num.size()];
+    for (int i = 0; i < num.size(); i++)
+    {
+        adj[i] = num[i];
+    }
+
+    std::vector<int> vis(num.size(), -1);
+
+    for (int i = 0; i < num.size(); i++)
+    {
+        if (vis[i] == -1)
+            traverse(adj, vis, i);
+    }
+
+    std::vector<int> res;
+    for (int i = 0; i < n; i++)
+        if (vis[i] == 0)
+            res.push_back(i); 
+        
+    return res;
     }
 };
