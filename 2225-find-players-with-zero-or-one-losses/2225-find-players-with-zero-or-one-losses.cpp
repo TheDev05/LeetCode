d77c9cb31fbe83d04091695b8792808c6eb338c3
Ankit D <a.singh725098@gmail.com>
2024-01-15 11:48:15 +0530

    Time: 607 ms (32.95%), Space: 186.3 MB (25.63%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+    vector<vector<int>> findWinners(vector<vector<int>>& num) {
         
-        map<int,int>lostOnlyOne;
-        vector<int>winners;
-        
-       for(int i=0;i<matches.size();i++)
-       {
-           winners.push_back(matches[i][0]);
-           lostOnlyOne[matches[i][1]]++;
-           
-       }
-        
-        set<int>temp1,temp2;
-        for(auto i: lostOnlyOne)
-        {
-            if(i.second==1)
-            {
-              temp2.insert(i.first);
-            }
-        }
-        
-        for(auto i: winners)
-        {
-            if(!lostOnlyOne.count(i))temp1.insert(i);
-        }
-        
-        vector<int>inox1,inox2;
-        for(auto i: temp1)
-        {
-            inox1.push_back(i);
-        }
-        
-        for(auto i: temp2)
-        {
-            inox2.push_back(i);
-        }
-        
-        vector<vector<int>>result;
-        
-        result.push_back(inox1);
-        result.push_back(inox2);
+    std::set<int> win;
+    std::map<int, int> lose;
 
-        return result;
+    for (auto i : num)
+        win.insert(i[0]), lose[i[1]]++;
+
+    std::vector<std::vector<int>>res(2);
+    for (auto i : win)
+        if (lose.contains(i) == false)
+            res[0].push_back(i);
+
+    for (auto i : lose)
+        if (i.second == 1)
+            res[1].push_back(i.first);
+                
+    return res;
     }
 };
