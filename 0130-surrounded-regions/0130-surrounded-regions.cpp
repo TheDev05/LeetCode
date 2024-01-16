fc22531052eee516cf60439c3eee27cb77c5de9e
Ankit D <a.singh725098@gmail.com>
2024-01-16 11:24:20 +0530

    Time: 11 ms (63.45%), Space: 11 MB (26.28%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,60 +1,64 @@
 class Solution {
 public:
+    void traverse(std::vector<std::vector<char>> &num, std::vector<std::vector<int>> &vis, int i, int j)
+    {
+        std::queue<std::pair<int, int>> inox;
+
+        int r[4] = {0, 0, -1, 1};
+        int c[4] = {-1, 1, 0, 0};
+
+        int n = num.size();
+        int m = num[0].size();
+
+        inox.push({i, j});
+        while (inox.size())
+        {
+            int row = inox.front().first;
+            int col = inox.front().second;
+
+            vis[row][col] = 1;
+            inox.pop();
+
+            for (int k = 0; k < 4; k++)
+            {
+                int trow = row + r[k];
+                int tcol = col + c[k];
+
+                if (trow >= 0 && trow < n && tcol >= 0 && tcol < m && num[trow][tcol] == 'O' && vis[trow][tcol] == 0)
+                    inox.push({trow, tcol}), vis[trow][tcol] = 1;
+            }
+        }
+    }
+    
     void solve(vector<vector<char>>& num) {
-    int n = num.size(), m = num[0].size();
         
+    int n = num.size(), m = num[0].size();
     std::vector<std::vector<int>> vis(n, std::vector<int>(m, 0));
-    std::queue<std::pair<int, int>> test;
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            if (i == 0 || j == m - 1 || i == n - 1 || j == 0)
-            {
-                if (num[i][j] == 'O')
-                {
-                    test.push({i, j});
-                    vis[i][j] = 1;
-                }
-            }
-        }
+        if (vis[i][0] == 0 && num[i][0] == 'O')
+            traverse(num, vis, i, 0);
+        if (vis[i][m - 1] == 0 && num[i][m - 1] == 'O')
+            traverse(num, vis, i, m - 1);
     }
 
-    int r[4] = {0, 0, -1, 1};
-    int c[4] = {-1, 1, 0, 0};
-
-    while (test.size())
+    for (int i = 0; i < m; i++)
     {
-        int row = test.front().first;
-        int col = test.front().second;
-
-        test.pop();
-
-        for (int k = 0; k < 4; k++)
-        {
-            int trow = row + r[k];
-            int tcol = col + c[k];
-
-            if (trow >= 0 && trow < n && tcol >= 0 && tcol < m && num[trow][tcol] == 'O' && vis[trow][tcol] == 0)
-            {
-                vis[trow][tcol] = 1;
-                test.push({trow, tcol});
-            }
-        }
+        if (vis[0][i] == 0 && num[0][i] == 'O')
+            traverse(num, vis, 0, i);
+        if (vis[n - 1][i] == 0 && num[n - 1][i] == 'O')
+            traverse(num, vis, n - 1, i);
     }
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            if (vis[i][j])
-                num[i][j] = 'O';
-            else
+            if (vis[i][j] == 0)
                 num[i][j] = 'X';
         }
-    } 
-        
+    }    
         
     }
 };
