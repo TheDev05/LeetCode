f281d8535fd05e87c857e60c704f3683a7c90a33
Ankit D <a.singh725098@gmail.com>
2024-01-24 23:46:55 +0530

    Time: 33 ms (68.58%), Space: 45.2 MB (53.65%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    int getCount(std::vector<std::string> &num, std::map<char, int> &exist, std::string text, int &count, int index)
-{
-    if (index >= num.size())
+    
+    int traverse(std::vector<std::string> &num, std::set<char> &data, int index)
     {
-        count = std::max(count, (int)text.size());
-        return 0;
+        if (index >= num.size())
+            return data.size();
+
+        bool ok = true;
+        for (auto i : num[index])
+        {
+            if (data.contains(i))
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        int val1 = 0, val2 = 0, size = data.size();
+        if (ok)
+        {
+            for (auto i : num[index])
+                data.insert(i);
+
+            if(data.size() == size + num[index].size())
+                val1 = traverse(num, data, index + 1);
+
+            for (auto i : num[index])
+                data.erase(i);
+        }
+
+        val2 = traverse(num, data, index + 1);
+        return std::max(val1, val2);
     }
-
-    int size = exist.size();
-    for (int i = 0; i < num[index].size(); i++)
-    {
-        exist[num[index][i]]++;
-    }
-
-    if (size + num[index].size() == exist.size())
-    {
-        getCount(num, exist, text + num[index], count, index + 1);
-    }
-
-    for (int i = 0; i < num[index].size(); i++)
-    {
-        exist[num[index][i]]--;
-        if (exist[num[index][i]] == 0)
-            exist.erase(num[index][i]);
-    }
-
-    getCount(num, exist, text, count, index + 1);
-    return 0;
-}
-
+    
     int maxLength(vector<string>& num) {
-    std::map<char, int> exist;
-    int count = 0;
-
-    std::string text;
-    getCount(num, exist, text, count, 0);
-
-    return count;
+        
+    std::set<char> data;
+    return traverse(num, data, 0);   
+        
     }
 };
