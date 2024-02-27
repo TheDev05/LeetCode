1fa2930e4bfbc2e99a4465283ea0cab31384076a
Ankit D <a.singh725098@gmail.com>
2024-02-28 02:21:41 +0530

    Time: 20 ms (41.19%), Space: 16.6 MB (64.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    bool traverse(std::vector<int> adj[], std::vector<int> &vis, int node)
+    {
+        vis[node] = -1;
+        for (auto i : adj[node])
+            if (vis[i] == 0)
+            {
+                if (traverse(adj, vis, i))
+                    return true;
+            }
+            else if (vis[i] == -1)
+                return true;
+
+        vis[node] = 1;
+        return false;
+    }
+
     bool canFinish(int n, vector<vector<int>>& num) {
 
     std::vector<int> adj[n];
@@ -8,38 +24,14 @@ public:
         adj[num[i][1]].push_back(num[i][0]);
     }
 
-    std::vector<int> indeg(n, 0);
+    std::vector<int> vis(n, 0);
     for (int i = 0; i < n; i++)
     {
-        for (auto j : adj[i])
-            indeg[j]++;
+        if (vis[i] == 0)
+            if (traverse(adj, vis, i))
+                return false;
     }
 
-    std::queue<int> inox;
-    for (int i = 0; i < n; i++)
-    {
-        if (indeg[i] == 0)
-            inox.push(i);
-    }
-
-    int count = 0;
-    while (inox.size())
-    {
-        int local = inox.front();
-
-        inox.pop();
-        count++;
-
-        for (auto i : adj[local])
-        {
-            indeg[i]--;
-            if (indeg[i] == 0)
-                inox.push(i);
-        }
-    }
-
-    if (count != n)
-        return false;
     return true;
         
     }
