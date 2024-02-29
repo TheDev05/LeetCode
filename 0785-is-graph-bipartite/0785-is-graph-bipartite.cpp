8e0c77324a0304a64557fc89133ebea08652e343
Ankit D <a.singh725098@gmail.com>
2024-02-29 23:50:23 +0530

    Time: 22 ms (29.92%), Space: 18.7 MB (7.71%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-bool traverse(std::vector<std::vector<int>> &num, std::vector<int> &inox, int index)
-{
-    for (auto i : num[index])
+    
+    bool traverse(std::vector<int> adj[], std::vector<int> &vis, int index, bool color)
     {
-        if (inox[i] == -1)
-        {
-            inox[i] = (inox[index]) ? 0 : 1;
-            if (traverse(num, inox, i))
+        vis[index] = color;
+        for (auto i : adj[index])
+            if (vis[i] == -1)
+            {
+                if (traverse(adj, vis, i, !color))
+                    return true;
+            }
+            else if (vis[i] == color)
                 return true;
-        }
-        else if (inox[index] == inox[i])
-            return true;
-    }
 
-    return false;
-}
+        return false;
+    }
     
     bool isBipartite(vector<vector<int>>& num) {
-    int n = num.size();
-        
-    std::vector<int> inox(n, -1);
-    for (int i = 0; i < n; i++)
+
+    std::vector<int> adj[num.size()];
+    for (int i = 0; i < num.size(); i++)
     {
-        if (inox[i] == -1)
-        {
-            inox[i] = 0;
-            if (traverse(num, inox, i))
-                return false;
-        }
+        for (auto j : num[i])
+            adj[i].push_back(j);
     }
 
-    return true;        
+    std::vector<int> vis(num.size(), -1);
+    for(int i = 0; i < num.size(); i++)
+        if(vis[i] == -1)
+            if(traverse(adj, vis, i, 0))
+                return false;
+
+    return true;   
     }
 };
